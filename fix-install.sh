96136b82abdb05dbdfd211bb7b5271102ecdb4fb
EduardoRFS <theeduardorfs@gmail.com>
2021-03-15 16:05:07 -0300

    use install stanza on dune instead of link

--- fix-install.sh
+++ fix-install.sh
@@ -13,6 +13,3 @@ mv stdlib-412/* .
 cd $cur__lib
 ln -s $cur__target_dir/default/lib/js .
 ln -s $cur__target_dir/default/lib/es6 .
-
-ln -s $cur__bin/bsc.exe $cur__bin/bsc
-ln -s $cur__bin/bsb.exe $cur__bin/bsb
