bf53cef8b78cc95b82cfc8712b34f9ed3158b79b
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2021-03-21 22:19:42 -0700

    Don't issue warning 34 (unused type) for `bs.deriving abstract`

--- jscomp/test/bs_abstract_test.ml
+++ jscomp/test/bs_abstract_test.ml
@@ -1,19 +1,19 @@
+[@@@config {flags = [|"-w";"+34";"-warn-error"; "A"|]}]
 
-
-type 'a linked_list = 
+type 'a linked_list =
   {
-    hd : 'a ; 
+    hd : 'a ;
     mutable tl : 'a linked_list Js.null
   }
   [@@bs.deriving abstract]
 
 
 
-let v = linked_list ~hd:3 ~tl:Js.null   
+let v = linked_list ~hd:3 ~tl:Js.null
 
 ;; tlSet v (Js.Null.return v)
 
-type t = int -> int -> bool [@bs]
+type[@warning "-34"] t = int -> int -> bool [@bs]
 and x = {
   k : t;
   y : string
@@ -26,42 +26,42 @@ let x1 k = x ~k ~y:"xx"
 let f = x ~k:(fun[@bs] x y -> x = y) ~y:"x"
 
 type u = {
-  x : int ; 
+  x : int ;
   y0 : int -> int;
-  y1 : int -> int -> int 
+  y1 : int -> int -> int
 } [@@bs.deriving abstract]
 
 
-let uf u =  u |. y0Get 1 
-let uf1 u = u |. y1Get 1 
+let uf u =  u |. y0Get 1
+let uf1 u = u |. y1Get 1
 let uf2 u = u |. y1Get 1 2
 
 type u1 = {
-  x : int; 
+  x : int;
   yyyy : (int -> int [@bs]);
   yyyy1 : (int -> int -> int  [@bs]);
   yyyy2 : int -> int  [@bs.optional]
 } [@@bs.deriving abstract]
 
-let uff f = 
+let uff f =
   (f |. yyyyGet) 1 [@bs]
 
-let uff2 f =   
+let uff2 f =
   (f |. yyyy1Get) 1 2 [@bs]
 
-let uff3 f =   
-  match f |. yyyy2Get with 
+let uff3 f =
+  match f |. yyyy2Get with
   | None -> 0
-  | Some x  -> x 0 
+  | Some x  -> x 0
 
 
 
 type u3 = {
-  x : int; 
+  x : int;
   yyyy : (int -> int [@bs]);
   yyyy1 : (int -> int -> int  [@bs]);
   yyyy2 : int -> int  [@bs.optional]
 } [@@bs.deriving { abstract = light} ]
 
 
-let fx v = v |. x 
+let fx v = v |. x
