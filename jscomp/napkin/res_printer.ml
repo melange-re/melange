0471d19f3d8fbf3f3b4642d84a101dbde2cefa11
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2021-03-24 23:44:10 -0700

    napkin: revert the rest of upstream PR#291
    https://github.com/rescript-lang/syntax/pull/291

--- jscomp/napkin/res_printer.ml
+++ jscomp/napkin/res_printer.ml
@@ -1468,9 +1468,17 @@ and printTypExpr (typExpr : Parsetree.core_type) cmtTbl =
     in
     Doc.concat [typ; Doc.text " as "; Doc.concat [Doc.text "'"; printIdentLike alias]]
 
+  | Ptyp_constr({txt = Longident.Ldot(Longident.Lident "Js", "t")}, [{ptyp_desc = Ptyp_object (_fields, _openFlag)} as typ]) ->
+    let bsObject = printTypExpr typ cmtTbl in
+    begin match typExpr.ptyp_attributes with
+    | [] -> bsObject
+    | attrs ->
+      Doc.concat [
+        printAttributes ~inline:true attrs cmtTbl;
+        printTypExpr typ cmtTbl;
+      ]
+    end
   (* object printings *)
-  | Ptyp_object (fields, openFlag) ->
-    printObject ~inline:false fields openFlag cmtTbl
   | Ptyp_constr(longidentLoc, [{ptyp_desc = Ptyp_object (fields, openFlag)}]) ->
     (* for foo<{"a": b}>, when the object is long and needs a line break, we
        want the <{ and }> to stay hugged together *)
@@ -1496,6 +1504,17 @@ and printTypExpr (typExpr : Parsetree.core_type) cmtTbl =
     let constrName = printLidentPath longidentLoc cmtTbl in
     begin match constrArgs with
     | [] -> constrName
+    | [{
+        Parsetree.ptyp_desc =
+          Ptyp_constr({txt = Longident.Ldot(Longident.Lident "Js", "t")},
+        [{ptyp_desc = Ptyp_object (fields, openFlag)}])
+      }] ->
+      Doc.concat([
+        constrName;
+        Doc.lessThan;
+        printObject ~inline:true fields openFlag cmtTbl;
+        Doc.greaterThan;
+      ])
     | _args -> Doc.group(
       Doc.concat([
         constrName;
@@ -1599,6 +1618,8 @@ and printTypExpr (typExpr : Parsetree.core_type) cmtTbl =
       )
     end
   | Ptyp_tuple types -> printTupleType ~inline:false types cmtTbl
+  | Ptyp_object (fields, openFlag) ->
+    printObject ~inline:false fields openFlag cmtTbl
   | Ptyp_poly([], typ) ->
     printTypExpr typ cmtTbl
   | Ptyp_poly(stringLocs, typ) ->
