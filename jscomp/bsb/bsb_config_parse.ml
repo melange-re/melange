melange-re/melange

commit 3926929d6b37e010474a90643d48ffdfbb651d9e

Remove pinned dependencies (#148)

jscomp/bsb/bsb_config_parse.ml
@@ -180,13 +180,6 @@ let extract_ignored_dirs (map : json_map) : Set_string .t =
     Set_string.of_list (Bsb_build_util.get_list_string content)
   | Some config ->
     Bsb_exception.config_error config "expect an array of string"
-let extract_pinned_dependencies (map : json_map) : Set_string.t =
-  match map.?(Bsb_build_schemas.pinned_dependencies) with
-  | None -> Set_string.empty
-  | Some (Arr {content}) ->
-    Set_string.of_list (Bsb_build_util.get_list_string content)
-  | Some config ->
-    Bsb_exception.config_error config "expect an array of string"
 
 let extract_generators (map : json_map) =
   let generators = ref Map_string.empty in
@@ -315,7 +308,6 @@ let rec interpret_json
 
     let package_specs = match package_kind with
       | Toplevel ->  Bsb_package_specs.from_map ~cwd:per_proj_dir map
-      | Pinned_dependency x
       | Dependency x -> x
     in
     let bs_dependencies =
@@ -328,15 +320,8 @@ let rec interpret_json
     let bs_dev_dependencies =
       match package_kind with
       | Toplevel
-      | Pinned_dependency _ ->
-        extract_dependencies
-          ~package_kind:(Dependency package_specs)
-          map
-          per_proj_dir
-          Bsb_build_schemas.bs_dev_dependencies
       | Dependency _ -> [] in
-    let pinned_dependencies =
-      extract_pinned_dependencies map in
+    
     begin match map.?(Bsb_build_schemas.sources) with
       | Some sources ->
         let cut_generators =
@@ -349,7 +334,6 @@ let rec interpret_json
             ~namespace
             sources in
         {
-          pinned_dependencies;
           gentype_config;
           package_name ;
           namespace ;
@@ -421,8 +405,6 @@ and extract_dependencies ~package_kind (map : json_map) cwd (field : string )
 let package_specs_from_bsconfig () =
   let json = Ext_json_parse.parse_json_from_file Literals.bsconfig_json in
   begin match json with
-    | Obj {map} ->
-      Bsb_package_specs.from_map ~cwd:Bsb_global_paths.cwd map,
-      extract_pinned_dependencies map
+    | Obj {map} -> Bsb_package_specs.from_map ~cwd:Bsb_global_paths.cwd map
     | _ -> assert false
   end

jscomp/bsb/bsb_build_schemas.ml
@@ -36,7 +36,6 @@ let bsc = "bsc"
 let bs_external_includes = "bs-external-includes"
 let bs_lib_dir = "bs-lib-dir"
 let bs_dependencies = "bs-dependencies"
-let pinned_dependencies = "pinned-dependencies"
 let bs_dev_dependencies = "bs-dev-dependencies"
 
 

jscomp/bsb/bsb_build_util.ml
@@ -198,8 +198,7 @@ let rec walk_all_deps_aux
   (paths : string list)
   ~(top : top)
   (dir : string)
-  (queue : _ Queue.t)
-  ~pinned_dependencies =
+  (queue : _ Queue.t) =
   let bsconfig_json =  dir // Literals.bsconfig_json in
   match Ext_json_parse.parse_json_from_file bsconfig_json with
   | Obj {map; loc} ->
@@ -240,8 +239,7 @@ let rec walk_all_deps_aux
                    let package_dir =
                      Bsb_pkg.resolve_bs_package ~cwd:dir
                        (Bsb_pkg_types.string_as_package   new_package) in
-                   walk_all_deps_aux visited package_stacks  ~top:(Expect_name new_package) package_dir queue
-                   ~pinned_dependencies ;
+                   walk_all_deps_aux visited package_stacks  ~top:(Expect_name new_package) package_dir queue;
                  | _ ->
                    Bsb_exception.errorf ~loc
                      "%s expect an array"
@@ -253,9 +251,6 @@ let rec walk_all_deps_aux
         begin match top with
           | Expect_none ->
             explore_deps Bsb_build_schemas.bs_dev_dependencies
-          | Expect_name n when
-              Set_string.mem pinned_dependencies n ->
-            explore_deps Bsb_build_schemas.bs_dev_dependencies
           | Expect_name _ -> ()
         end;
         Queue.add {top ; proj_dir = dir} queue;
@@ -264,8 +259,8 @@ let rec walk_all_deps_aux
   | _ -> ()
 
 
-let walk_all_deps dir ~pinned_dependencies : package_context Queue.t =
+let walk_all_deps dir : package_context Queue.t =
   let visited = Hash_string.create 0 in
   let cb = Queue.create () in
-  walk_all_deps_aux visited [] ~top:Expect_none dir cb ~pinned_dependencies;
+  walk_all_deps_aux visited [] ~top:Expect_none dir cb;
   cb

jscomp/bsb/bsb_build_util.mli
@@ -115,6 +115,5 @@ type package_context = {
 
 val walk_all_deps :
   string ->
-  pinned_dependencies:Set_string.t ->
   package_context Queue.t
 

jscomp/bsb/bsb_clean.ml
@@ -62,8 +62,7 @@ let clean_bs_garbage proj_dir =
 
 let clean_bs_deps  proj_dir =
   dune_clean  proj_dir ;
-  let _, pinned_dependencies = Bsb_config_parse.package_specs_from_bsconfig () in
-  let queue = Bsb_build_util.walk_all_deps  proj_dir ~pinned_dependencies in
+  let queue = Bsb_build_util.walk_all_deps  proj_dir in
   Queue.iter (fun (pkg_cxt : Bsb_build_util.package_context )->
       (* whether top or not always do the cleaning *)
       clean_bs_garbage  pkg_cxt.proj_dir

jscomp/bsb/bsb_config_parse.mli
@@ -24,7 +24,7 @@
 
 val package_specs_from_bsconfig : 
     unit -> 
-    Bsb_package_specs.t * Set_string.t
+    Bsb_package_specs.t
 
 
 

jscomp/bsb/bsb_config_types.ml
@@ -62,7 +62,6 @@ type t =
     pp_file : string option;
     bs_dependencies : dependencies;
     bs_dev_dependencies : dependencies;
-    pinned_dependencies : Set_string.t;
     built_in_dependency : bool;
     warning : Bsb_warning.t;
     (*TODO: maybe we should always resolve bs-platform

jscomp/bsb/bsb_package_kind.ml
@@ -25,7 +25,6 @@
 type t = 
     | Toplevel
     | Dependency of Bsb_package_specs.t 
-    | Pinned_dependency of Bsb_package_specs.t 
         (*  This package specs comes from the toplevel to 
             override the current settings
         *)

commit c15b8abae39fbbd78f937b3fae0afbebbaf5eb96

Fix regression introduced in #148

jscomp/bsb/bsb_config_parse.ml
@@ -322,7 +322,12 @@ let rec interpret_json
     in
     let bs_dev_dependencies =
       match package_kind with
-      | Toplevel
+      | Toplevel ->
+        extract_dependencies
+          ~package_kind:(Dependency package_specs)
+          map
+          per_proj_dir
+          Bsb_build_schemas.bs_dev_dependencies
       | Dependency _ -> [] in
 
     begin match map.?(Bsb_build_schemas.sources) with