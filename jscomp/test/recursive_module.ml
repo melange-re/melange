8c00d9edb3a8b8417415b000f38b6b7afd2717af
Hongbo Zhang <bob.hongbo.zhang@foxmail.com>
2021-05-19 15:58:59 +0800

    determine if is an js external based on both attributes and primitive names
    We don't take const inline as rs external,
    Note the parsetree is a bit inconsistent:
    
    ```
    @@inline
    let x = 3
    ```
    => Pstr_value (Nonrecursive, ..)
    => Pstr_primitive (value_description) for external
    ```
    @@inline(3)
    let x : int
    ```
    => Psig_value (value_description)

--- jscomp/test/recursive_module.ml
+++ jscomp/test/recursive_module.ml
@@ -1,59 +1,59 @@
 let suites :  Mt.pair_suites ref  = ref []
 let test_id = ref 0
-let eq loc x y = Mt.eq_suites ~test_id ~suites loc x y 
+let eq loc x y = Mt.eq_suites ~test_id ~suites loc x y
 
 
 module  rec Int32 : sig
   type t
   type buffer
-  external buffer : t -> buffer = "buffer" [@@js.get]
-  external get : t -> int -> int  = "" [@@js.get_index]
-  external set : t -> int -> int -> unit = "" [@@js.set_index]
-  external create : int array -> t = "Int32Array" [@@js.new]
-  external of_buffer : buffer -> t = "Int32Array" [@@js.new]
+  external buffer : t -> buffer = "buffer" [@@bs.get]
+  external get : t -> int -> int  = "" [@@bs.get_index]
+  external set : t -> int -> int -> unit = "" [@@bs.set_index]
+  external create : int array -> t = "Int32Array" [@@bs.new]
+  external of_buffer : buffer -> t = "Int32Array" [@@bs.new]
 end = Int32 (* Int32 is compiled away in 4.06 *)
 
 module Xx : sig
-  val f : int -> int -> int 
-end = struct 
-  external f : int -> int -> int = "hfiehi"
-end 
+  val f : int -> int -> int
+end = struct
+  external f : int -> int -> int = "caml_hfiehi"
+end
 
-let uuu = Xx.f 
+let uuu = Xx.f
 
 
 module  rec Int3 : sig
-  val u : int -> int 
+  val u : int -> int
 end = Int3
 
-module rec Inta : sig 
+module rec Inta : sig
   val a : int lazy_t
-end = struct 
+end = struct
   let a = lazy (Lazy.force Intb.a)
-end 
-and Intb : sig 
+end
+and Intb : sig
   val a : int lazy_t
-end = struct 
+end = struct
   let a = lazy (Lazy.force Inta.a + 1)
-end 
+end
 
-;; 
-eq __LOC__ 
+;;
+eq __LOC__
   (-1)
   (try Lazy.force (Intb.a) with Lazy.Undefined -> -1)
 
-module A = struct   
-  module rec Inta : sig 
+module A = struct
+  module rec Inta : sig
     val a : int lazy_t
-  end = struct 
+  end = struct
     let a = lazy (Lazy.force Intb.a + 1)
-  end 
-  and Intb : sig 
+  end
+  and Intb : sig
     val a : int lazy_t
-  end = struct 
+  end = struct
     let a = lazy 2
-  end 
-end 
+  end
+end
 
 ;; eq __LOC__ (Lazy.force A.Inta.a)  3
 (* expect raise Undefined_recursive_module *)
@@ -62,5 +62,5 @@ end
   with Undefined_recursive_module _ -> 4)
 
 
-let () = 
+let () =
   Mt.from_pair_suites __MODULE__ !suites
