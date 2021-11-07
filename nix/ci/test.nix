b3b2dc5b655d6d43110023e17337aaf43f13e82c
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2021-11-07 14:39:19 -0800

    tweak

--- nix/ci/test.nix
+++ nix/ci/test.nix
@@ -10,16 +10,18 @@ stdenv.mkDerivation {
 
   src = ../..;
 
-  # inputString = builtins.unsafeDiscardStringContext melange.outPath;
+  inputString = builtins.unsafeDiscardStringContext melange.outPath;
+
   # https://blog.eigenvalue.net/nix-rerunning-fixed-output-derivations/
   # the dream of running fixed-output-derivations is dead -- somehow after
   # Nix 2.4 it results in `error: unexpected end-of-file`.
   # Example: https://github.com/melange-re/melange/runs/4132970590
-  # outputHashMode = "flat";
-  # outputHashAlgo = "sha256";
-  # outputHash = builtins.hashString "sha256" inputString;
+
+  outputHashMode = "flat";
+  outputHashAlgo = "sha256";
+  outputHash = builtins.hashString "sha256" "melange";
   installPhase = ''
-    touch $out
+    echo -n melange > $out
   '';
 
   phases = [ "unpackPhase" "checkPhase" "installPhase" ];
@@ -35,14 +37,16 @@ stdenv.mkDerivation {
 
   checkPhase = ''
     # check that running `node scripts/ninja.js config` produces an empty diff.
+    dune exec jscomp/main/js_main.exe
     node scripts/ninja.js config
+
     git diff --exit-code
 
     # https://github.com/yarnpkg/yarn/issues/2629#issuecomment-685088015
     yarn install --frozen-lockfile --check-files --cache-folder .ycache && rm -rf .ycache
 
     # `--release` to avoid promotion
-    dune build --release --display=short -j $NIX_BUILD_CORES @jscomp/test/all
+    rm -rf _build && dune build --release --display=short -j $NIX_BUILD_CORES @jscomp/test/all
     node ./node_modules/.bin/mocha "_build/default/jscomp/test/**/*_test.js"
 
     dune runtest -p ${melange.name} -j $NIX_BUILD_CORES --display=short
