f71f794e8ebc0de2b20d7726250aebb9209029ae
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2021-11-07 15:18:17 -0800

    wip

--- nix/ci/test.nix
+++ nix/ci/test.nix
@@ -39,14 +39,15 @@ stdenv.mkDerivation {
     # https://github.com/yarnpkg/yarn/issues/2629#issuecomment-685088015
     yarn install --frozen-lockfile --check-files --cache-folder .ycache && rm -rf .ycache
 
+    # `--release` to avoid promotion
+    rm -rf _build && dune build --release --display=short -j $NIX_BUILD_CORES @jscomp/test/all
+
     # check that running `node scripts/ninja.js config` produces an empty diff.
     # dune build jscomp/main/js_main.exe
     node scripts/ninja.js config
 
     git diff --exit-code
 
-    # `--release` to avoid promotion
-    rm -rf _build && dune build --release --display=short -j $NIX_BUILD_CORES @jscomp/test/all
     node ./node_modules/.bin/mocha "_build/default/jscomp/test/**/*_test.js"
 
     dune runtest -p ${melange.name} -j $NIX_BUILD_CORES --display=short
