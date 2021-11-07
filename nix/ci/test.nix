bdb005a9a24d48eccd58879a597f22970d8cb916
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2021-11-07 13:49:39 -0800

    random

--- nix/ci/test.nix
+++ nix/ci/test.nix
@@ -20,7 +20,8 @@ stdenv.mkDerivation rec {
   installPhase = ''
     runHook preInstall
     echo INSTALLING $inputString $out
-    echo -n $inputString > $out
+    # echo -n $inputString > $out
+    echo -n melange > $out
     echo fails before
   '';
 
