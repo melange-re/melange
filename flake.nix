e3f1d324b3e3c143a5559a4aeadf5ab125f0a1f8
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2022-02-12 01:04:55 -0800

    correct flake

--- flake.nix
+++ flake.nix
@@ -2,8 +2,8 @@
   description = "Piaf Nix Flake";
 
   inputs.flake-utils.url = "github:numtide/flake-utils";
-  # inputs.nixpkgs.url = "github:anmonteiro/nix-overlays";
-  inputs.nixpkgs.url = "/Users/anmonteiro/projects/nix-overlays";
+  inputs.nixpkgs.url = "github:anmonteiro/nix-overlays";
+  # inputs.nixpkgs.url = "/Users/anmonteiro/projects/nix-overlays";
 
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
