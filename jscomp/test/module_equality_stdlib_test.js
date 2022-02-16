618754d1f91bcd4de45fc46684b32c872e3287f0
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2022-02-16 15:10:17 -0800

    add test

---
+++ jscomp/test/module_equality_stdlib_test.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var Format = require("../../lib/js/format.js");
+
+function write_runtime_coverage(channel) {
+  Format.formatter_of_out_channel(channel);
+}
+
+exports.write_runtime_coverage = write_runtime_coverage;
+/* Format Not a pure module */
