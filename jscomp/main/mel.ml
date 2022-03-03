f7e378b78ad52c1ed3dfec1f6a24ebe0c46e877d
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2022-03-02 20:20:28 -0800

    New `mel` command line interface (#245)

--- jscomp/main/mel.ml
+++ jscomp/main/mel.ml
@@ -26,28 +26,6 @@ open Cmdliner
 open Bsb
 
 let () = Bsb_log.setup ()
-let bs_version_string = Bs_version.version
-
-type cli_options = {
-  print_version : bool;
-  verbose : bool;
-  watch_mode : bool;
-  clean : bool;
-  make_world : bool;
-  install : bool;
-  init_path : string option;
-  theme : string;
-  list_themes : bool;
-  print_bsb_location : bool;
-  websocket : bool;
-  dune_args : string array;
-}
-
-let print_version_string () =
-  print_string bs_version_string;
-  print_newline ();
-  exit 0
-
 let ( // ) = Ext_path.combine
 
 let output_dune_project_if_does_not_exist proj_dir =
@@ -90,135 +68,133 @@ let build_whole_project () =
   output_dune_file buf;
   config :: dep_configs
 
-let print_init_theme_notice () =
-  prerr_endline
-    "The subcommands -init, -theme and -themes are deprecated now. Use the \
-     template at https://github.com/melange-re/melange-basic-template to start \
-     a new project.";
-  flush stderr
+module Common_opts = struct
+  open Cmdliner
 
-let run_bsb ({ make_world; install; watch_mode; dune_args; _ } as options) =
-  let cwd = Bsb_global_paths.cwd in
-  try
-    if options.print_version then print_version_string ();
-    if options.verbose then Bsb_log.verbose ();
-    if options.clean then Bsb_clean.clean ~dune_args cwd;
-    if options.print_bsb_location then
-      print_endline (Filename.dirname Sys.executable_name);
-    match (options.init_path, options.list_themes) with
-    | Some _, _ | _, true -> print_init_theme_notice ()
-    | None, false ->
-        let generate_dune_bsb = make_world || install in
-        if (not make_world) && not install then (if watch_mode then exit 0)
-        else (
-          (if generate_dune_bsb then
-           let configs = build_whole_project () in
-           Bsb_world.install_targets Bsb_global_paths.cwd configs);
-          if watch_mode then exit 0
-          else if make_world then dune_command_exit dune_args)
-  with
-  | Bsb_exception.Error e ->
-      Bsb_exception.print Format.err_formatter e;
-      Format.pp_print_newline Format.err_formatter ();
-      exit 2
-  | Ext_json_parse.Error (start, _, e) ->
-      Format.fprintf Format.err_formatter
-        "File %S, line %d\n@{<error>Error:@} %a@." start.pos_fname
-        start.pos_lnum Ext_json_parse.report_error e;
-      exit 2
-  | Sys_error s ->
-      Format.fprintf Format.err_formatter "@{<error>Error:@} %s@." s;
-      exit 2
-  | e -> Ext_pervasives.reraise e
+  type t = { verbose : bool }
 
-module CLI = struct
-  let version_flag =
-    let doc = "Print version and exit" in
-    Arg.(value & flag & info [ "v"; "version" ] ~doc)
+  let make verbose = { verbose }
 
   let verbose_flag =
-    let doc = "Set the output (from bsb) to be verbose" in
-    Arg.(value & flag & info [ "verbose" ] ~doc)
+    let doc = "Print debug output" in
+    Arg.(value & flag & info [ "v"; "verbose" ] ~doc)
 
+  let copts_t = Term.(const make $ verbose_flag)
+end
+
+type cli_options = { where : bool }
+
+module Actions = struct
+  let wrap_bsb ~opts:{ Common_opts.verbose } ~f =
+    if verbose then Bsb_log.verbose ();
+    try f () with
+    | Bsb_exception.Error e ->
+        Bsb_exception.print Format.err_formatter e;
+        Format.pp_print_newline Format.err_formatter ();
+        exit 2
+    | Ext_json_parse.Error (start, _, e) ->
+        Format.fprintf Format.err_formatter
+          "File %S, line %d\n@{<error>Error:@} %a@." start.pos_fname
+          start.pos_lnum Ext_json_parse.report_error e;
+        exit 2
+    | Sys_error s ->
+        Format.fprintf Format.err_formatter "@{<error>Error:@} %s@." s;
+        exit 2
+    | e -> Ext_pervasives.reraise e
+
+  let do_output_rules () =
+    let configs = build_whole_project () in
+    Bsb_world.install_targets Bsb_global_paths.cwd configs
+
+  let build opts watch_mode dune_args =
+    wrap_bsb ~opts ~f:(fun () ->
+        do_output_rules ();
+        if watch_mode then (
+          prerr_endline "watch mode not supported yet";
+          exit 0)
+        else dune_command_exit dune_args)
+
+  let rules opts = wrap_bsb ~opts ~f:do_output_rules
+
+  let clean opts dune_args =
+    wrap_bsb ~opts ~f:(fun () ->
+        Bsb_clean.clean ~dune_args Bsb_global_paths.cwd)
+
+  let default opts { where } dune_args =
+    if where then print_endline (Filename.dirname Sys.executable_name)
+    else build opts false dune_args
+end
+
+module Commands = struct
   let watch_mode_flag =
     let doc = "Currently does nothing. Enabled for compatibility." in
     Arg.(value & flag & info [ "w" ] ~doc)
 
-  let clean_flag =
-    let doc = "Clean all bs dependencies" in
-    Arg.(value & flag & info [ "clean"; "clean-world" ] ~doc)
-
-  let make_world_flag =
-    let doc = "Build all dependencies and itself" in
-    Arg.(value & flag & info [ "make-world" ] ~doc)
-
-  let install_flag =
-    let doc = "Generate the (dune) rules for building the project" in
-    Arg.(value & flag & info [ "install" ] ~doc)
-
-  let init_arg =
-    let docv = "init path" in
-    let doc =
-      "Currently does nothing. Enabled for compatibility. Use the template at \
-       https://github.com/melange-re/melange-basic-template to start a new \
-       project."
+  let build dune_args =
+    let doc = "build the project in the current directory" in
+    let man =
+      [
+        `S Manpage.s_description;
+        `P "Builds the Melange project in the current directory.";
+      ]
     in
-    Arg.(value & opt (some string) None & info [ "init" ] ~doc ~docv)
+    let info = Cmd.info "build" ~doc ~man in
+    Cmd.v info
+      Term.(
+        const Actions.build $ Common_opts.copts_t $ watch_mode_flag
+        $ const dune_args)
 
-  let theme_arg =
-    let docv = "theme" in
-    let doc =
-      "Currently does nothing. Enabled for compatibility. Use the template at \
-       https://github.com/melange-re/melange-basic-template to start a new \
-       project."
+  let rules (_dune_args : string array) =
+    let doc = "Output the dune rules necessary to build the project" in
+    let man =
+      [
+        `S Manpage.s_description;
+        `P
+          "Outputs the dune rules necessary to build the project in the \n\
+           in the current directory and exits. This command does NOT build \n\
+           the project.";
+      ]
     in
-    Arg.(value & opt string "basic" & info [ "theme" ] ~doc ~docv)
+    let info = Cmd.info "rules" ~doc ~man in
+    Cmd.v info Term.(const Actions.rules $ Common_opts.copts_t)
 
-  let themes_flag =
-    let doc =
-      "Currently does nothing. Enabled for compatibility. Use the template at \
-       https://github.com/melange-re/melange-basic-template to start a new \
-       project."
+  let clean dune_args =
+    let doc = "Clean the project" in
+    let man =
+      [
+        `S Manpage.s_description;
+        `P "Cleans the project in the current directory and exits.";
+      ]
     in
-    Arg.(value & flag & info [ "themes" ] ~doc)
+    let info = Cmd.info "clean" ~doc ~man in
+    Cmd.v info
+      Term.(const Actions.clean $ Common_opts.copts_t $ const dune_args)
 
+  let commands = [ build; rules; clean ]
+end
+
+module CLI = struct
   let where_flag =
     let doc = "Show where bsb is located" in
     Arg.(value & flag & info [ "where" ] ~doc)
 
-  let ws_flag =
-    let doc = "Send build output to websocket" in
-    Arg.(value & flag & info [ "ws" ] ~doc)
+  let parse_options where = { where }
 
-  let parse_options print_version verbose watch_mode clean make_world install
-      init_path theme list_themes print_bsb_location websocket dune_args =
-    {
-      print_version;
-      verbose;
-      watch_mode;
-      clean;
-      make_world;
-      install;
-      init_path;
-      theme;
-      list_themes;
-      print_bsb_location;
-      websocket;
-      dune_args;
-    }
+  let default dune_args =
+    let make_bsb_options = Term.(const parse_options $ where_flag) in
+    Term.(
+      const Actions.default $ Common_opts.copts_t $ make_bsb_options
+      $ const dune_args)
 
   let run dune_args =
-    let make_bsb_options =
-      Term.(
-        const parse_options $ version_flag $ verbose_flag $ watch_mode_flag
-        $ clean_flag $ make_world_flag $ install_flag $ init_arg $ theme_arg
-        $ themes_flag $ where_flag $ ws_flag $ const dune_args)
-    in
-    (Term.(const run_bsb $ make_bsb_options), Term.info "bsb")
+    let open Cmdliner in
+    let doc = "the Melange buildpack" in
+    let info = Cmd.info "mel" ~version:Bs_version.version ~doc in
+    let default = default dune_args in
+    Cmd.group info ~default (List.map (fun f -> f dune_args) Commands.commands)
 end
 
 let () =
   let bsb_args, dune_args = Ext_cli_args.split_argv_at_separator Sys.argv in
-  Term.(
-    exit
-    @@ eval ~argv:(Ext_cli_args.normalize_argv bsb_args) (CLI.run dune_args))
+  exit
+    (Cmd.eval ~argv:(Ext_cli_args.normalize_argv bsb_args) (CLI.run dune_args))
