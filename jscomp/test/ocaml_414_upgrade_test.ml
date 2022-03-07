66515318edd1db1dc34075201b6f0cbef2d6ddcc
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2022-03-07 12:02:12 -0800

    Add ocaml_414_upgrade_test

---
+++ jscomp/test/ocaml_414_upgrade_test.ml
@@ -0,0 +1,24 @@
+(* Tests a possible regression / deoptimization when upgrading to 4.14 *)
+module N = struct
+  type t = { x : int}
+end
+
+module type X = sig
+  type 'a f = {i : 'a }
+  val forEach : 'a array ->  ('a -> unit) f -> unit [@bs]
+end
+module Y = struct
+  type 'a f = { i : 'a}
+  let forEach = fun [@bs] xs f ->
+    Belt.Array.forEach xs (( fun  x -> f.i x )  )
+end
+
+
+let f (module X : X) (xs : N.t array) =
+  X.forEach xs ({ X.i = fun x -> Js.log x.x} ) [@bs]
+
+let g = fun  (module X: X) xs ->
+  (Belt.Array.forEach xs (fun x -> Js.log x) )
+
+
+let g_result = g (module Y) [||]
