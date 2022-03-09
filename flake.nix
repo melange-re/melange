b34fb60e1ec84428200750e256b46231cb9fab2a
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2022-03-08 17:42:20 -0800

    sort out flake.lock

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.nixpkgs.url = "github:anmonteiro/nix-overlays";
-  # inputs.nixpkgs.url = "/Users/anmonteiro/projects/nix-overlays";
+  # inputs.nixpkgs.url = "/Users/anmonteiro/monorepo/nix-overlays";
 
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
