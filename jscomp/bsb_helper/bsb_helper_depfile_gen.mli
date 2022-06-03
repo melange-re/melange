530e124b7f46608fafec21ecd0afd5fe0c841a54
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2022-06-03 14:47:40 -0700

    Convert meldep to cmdliner (#294)
    * Convert meldep to cmdliner
    
    * fix
    
    * -bs-ns

--- jscomp/bsb_helper/bsb_helper_depfile_gen.mli
+++ jscomp/bsb_helper/bsb_helper_depfile_gen.mli
@@ -23,9 +23,9 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. *)
 
 val emit_d :
-  cwd:string ->
-  root:string ->
-  bool ->
+  cur_dir:string ->
+  proj_dir:string ->
+  is_dev:bool ->
   string option ->
   string ->
   string ->
