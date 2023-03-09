dbea2695ae99242d1e7f525a1d85ea8b7db5d7a5
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2023-03-09 09:34:16 -0800

    chore(nix): add the required version of dune to the overlay

--- nix/overlay.nix
+++ nix/overlay.nix
@@ -3,7 +3,18 @@
 final: prev:
 
 {
-  ocamlPackages = prev.ocamlPackages.overrideScope' (oself: osuper:
-    (melange-compiler-libs.overlays.default final prev).ocamlPackages //
-    (prev.callPackage ./. { inherit nix-filter; }));
+  ocamlPackages = (prev.ocamlPackages.overrideScope' (oself: osuper:
+    {
+      dune_3 = osuper.dune_3.overrideAttrs (_: {
+        src = prev.fetchFromGitHub {
+          owner = "ocaml";
+          repo = "dune";
+          rev = "c7a0049e24e6d802a46f7ed34abf42bc525bf89d";
+          hash = "sha256-6TF0wYbRhoqoEdZRrT06Zu8l9gNUtJhCUoVunJQdiVo=";
+        };
+      });
+    })).overrideScope' (oself: osuper:
+    {
+      melange-compiler-libs = (melange-compiler-libs.overlays.default final prev).ocamlPackages.melange-compiler-libs;
+    } // (prev.callPackage ./. { inherit nix-filter; }));
 }
