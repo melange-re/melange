87b1fff7cd7c939c42097261481118c77a9d2a64
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2023-04-06 01:28:10 -0700

    Revert some js_implementation code

--- jscomp/core/js_implementation.ml
+++ jscomp/core/js_implementation.ml
@@ -12,22 +12,6 @@
 
 (* adapted by rescript from [driver/compile.ml] for convenience    *)
 
-module Run_ppx = struct
-  let apply_rewriters (type a) ~(kind : a Ml_binary.kind) (ast : a) : a =
-    (* match (!Js_config.as_pp, !Clflags.all_ppx) with *)
-    (* | true, [] -> ast *)
-    (* | _ -> *)
-    (* (match kind with *)
-    (* | Mli -> *)
-    (* Bs_ast_invariant.iter_warnings_on_sigi ast; *)
-    (* Ast_config.iter_on_bs_config_sigi ast *)
-    (* | Ml -> *)
-    (* Bs_ast_invariant.iter_warnings_on_stru ast; *)
-    (* Ast_config.iter_on_bs_config_stru ast); *)
-    Cmd_ppx_apply.apply_rewriters ~restore:false ~tool_name:Js_config.tool_name
-      kind ast
-end
-
 let module_of_filename outputprefix =
   let basename = Filename.basename outputprefix in
   let name =
@@ -118,7 +102,8 @@ let after_parsing_sig ppf outputprefix ast =
 let interface ~parser ppf fname =
   Res_compmisc.init_path ();
   parser fname |> Ast_deriving_compat.signature
-  |> Run_ppx.apply_rewriters ~kind:Mli
+  |> Cmd_ppx_apply.apply_rewriters ~restore:false ~tool_name:Js_config.tool_name
+       Mli
   |> Melange_ppx_lib.Ppx_entry.rewrite_signature
   |> print_if_pipe ppf Clflags.dump_parsetree Printast.interface
   |> print_if_pipe ppf Clflags.dump_source Pprintast.signature
@@ -209,7 +194,8 @@ let after_parsing_impl ppf fname (ast : Parsetree.structure) =
 let implementation ~parser ppf fname =
   Res_compmisc.init_path ();
   parser fname |> Ast_deriving_compat.structure
-  |> Run_ppx.apply_rewriters ~kind:Ml
+  |> Cmd_ppx_apply.apply_rewriters ~restore:false ~tool_name:Js_config.tool_name
+       Ml
   |> Melange_ppx_lib.Ppx_entry.rewrite_implementation
   |> print_if_pipe ppf Clflags.dump_parsetree Printast.implementation
   |> print_if_pipe ppf Clflags.dump_source Pprintast.structure
