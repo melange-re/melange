fc2ac5f605ff1d4c33052a2064b19807929cb098
Javier Chavarri <javier.chavarri@gmail.com>
2023-06-10 19:07:51 +0000

    use vlib for bs_hash_stubs

---
+++ jscomp/hash_stubs/bs_hash_stubs.mli
@@ -0,0 +1,7 @@
+val hash_string : string -> int
+val hash_string_int : string -> int -> int
+val hash_string_small_int : string -> int -> int
+val hash_stamp_and_name : int -> string -> int
+val hash_int : int -> int
+val string_length_based_compare : string -> string -> int
+val int_unsafe_blit : int array -> int -> int array -> int -> int -> unit
