827c7055a7b802b0205601d1538c9b1b0ebb0983
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2023-06-12 18:23:13 -0700

    wip

--- jscomp/runtime/js.pre.ml
+++ jscomp/runtime/js.pre.ml
@@ -233,95 +233,66 @@ external unsafe_ge : 'a -> 'a -> bool = "#unsafe_ge"
 (*MODULE_ALIASES*)
 module Null = Js_null
 (** Provide utilities around ['a null] *)
-
 module Undefined = Js_undefined
 (** Provide utilities around {!undefined} *)
-
 module Nullable = Js_null_undefined
 (** Provide utilities around {!null_undefined} *)
-
 module Null_undefined = Js_null_undefined
 (** @deprecated please use {!Js.Nullable} *)
-
 module Exn = Js_exn
 (** Provide utilities for dealing with Js exceptions *)
-
 module Array = Js_array
 (** Provide bindings to Js array*)
-
 module Array2 = Js_array2
 (** Provide bindings to Js array*)
-
 module String = Js_string
 (** Provide bindings to JS string *)
-
 module String2 = Js_string2
 (** Provide bindings to JS string *)
-
 module Re = Js_re
 (** Provide bindings to Js regex expression *)
-
 module Promise = Js_promise
 (** Provide bindings to JS promise *)
-
 module Date = Js_date
 (** Provide bindings for JS Date *)
-
 module Dict = Js_dict
 (** Provide utilities for JS dictionary object *)
-
 module Global = Js_global
 (** Provide bindings to JS global functions in global namespace*)
-
 module Json = Js_json
 (** Provide utilities for json *)
-
 module Math = Js_math
 (** Provide bindings for JS [Math] object *)
-
 module Obj  = Js_obj
 (** Provide utilities for {!Js.t} *)
-
 module Typed_array = Js_typed_array
 (** Provide bindings for JS typed array *)
-
 module TypedArray2 = Js_typed_array2
 (** Provide bindings for JS typed array *)
-
 module Types = Js_types
 (** Provide utilities for manipulating JS types  *)
-
 module Float = Js_float
 (** Provide utilities for JS float *)
-
 module Int = Js_int
 (** Provide utilities for int *)
-
 module Bigint = Js_bigint
 (** Provide utilities for bigint *)
-
 module Option = Js_option
 (** Provide utilities for option *)
-
 module Result = Js_result
 (** Define the interface for result *)
-
 module List = Js_list
 (** Provide utilities for list *)
-
 module Vector = Js_vector
+[@@alert deprecated "Use Belt.Array instead" ]
 
 module Console = Js_console
-
 module Set = Js_set
 (** Provides bindings for ES6 Set *)
-
 module WeakSet = Js_weakset
 (** Provides bindings for ES6 WeakSet *)
-
 module Map = Js_map
 (** Provides bindings for ES6 Map *)
-
 module WeakMap = Js_weakmap
 (** Provides bindings for ES6 WeakMap *)
 (**/**)
