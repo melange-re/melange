b6a35f2b976874a2f4b5b06efd9f69adb7ae7e86
Javier Chávarri <javier.chavarri@gmail.com>
2023-07-11 09:08:57 +0200

    apply review changes
    Co-authored-by: Antonio Nuno Monteiro <anmonteiro@gmail.com>

--- jscomp/ext/ext_js_suffix.ml
+++ jscomp/ext/ext_js_suffix.ml
@@ -5,16 +5,14 @@ let to_string = Fun.id
 let of_string (x : string) : t =
   match String.length x with
   | 0 -> raise (Invalid_argument "File extension can not be empty")
-  | _ -> (
-      let first = String.get x 0 in
-      match first with
+  | length -> (
+      match String.unsafe_get x 0 with
       | '.' -> (
-          let last = String.get x (String.length x - 1) in
-          match last with
+          match String.unsafe_get x (length - 1) with
           | '.' ->
               raise
                 (Invalid_argument
-                   (Printf.sprintf "File extension %s does not end with '.'" x))
+                   (Printf.sprintf "File extension %s cannot end with '.'" x))
           | _ -> x)
       | _ ->
           raise
