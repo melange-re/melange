d4868a5300c8c6e9f1b387aedb85ded4a705bc0a
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2023-07-13 21:39:47 -0700

    nix: improve overlay to avoid duplicate melange packages (#660)
    * nix: improve overlay to avoid duplicate melange packages
    
    * fix test

--- flake.nix
+++ flake.nix
@@ -8,12 +8,20 @@
       url = "github:nix-ocaml/nix-overlays";
       inputs.flake-utils.follows = "flake-utils";
     };
+    melange-compiler-libs = {
+      # this changes rarely, and it's better than having to rely on nix's poor
+      # support for submodules
+      url = "github:melange-re/melange-compiler-libs/38bb9de36cb1f0af85b039943521c3e1244dbcb9";
+      inputs.flake-utils.follows = "flake-utils";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils, nix-filter }:
+  outputs = { self, nixpkgs, flake-utils, nix-filter, melange-compiler-libs }:
     {
       overlays.default = import ./nix/overlay.nix {
         nix-filter = nix-filter.lib;
+        melange-compiler-libs-vendor-dir = melange-compiler-libs;
       };
     } // (flake-utils.lib.eachDefaultSystem (system:
       let
@@ -22,11 +30,26 @@
             ocamlPackages = super.ocaml-ng.ocamlPackages_4_14;
           })
         ];
+
+        melange = pkgs.callPackage ./nix {
+          nix-filter = nix-filter.lib;
+          melange-compiler-libs-vendor-dir = melange-compiler-libs;
+        };
       in
 
       rec {
-        packages = pkgs.callPackage ./nix { nix-filter = nix-filter.lib; } // {
-          default = packages.melange;
+        packages = {
+          inherit melange;
+          default = melange;
+          rescript-syntax = pkgs.ocamlPackages.callPackage ./nix/rescript-syntax.nix {
+            inherit melange;
+            nix-filter = nix-filter.lib;
+          };
+          melange-playground = pkgs.ocamlPackages.callPackage ./nix/melange-playground.nix {
+            inherit melange;
+            nix-filter = nix-filter.lib;
+            melange-compiler-libs-vendor-dir = melange-compiler-libs;
+          };
         };
 
         devShells = {
