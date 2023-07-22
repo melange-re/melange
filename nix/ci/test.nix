234fb926fb9c45802d8e9b42e9072f7532e12b5d
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2023-07-21 21:17:35 -0700

    Update test.nix

--- nix/ci/test.nix
+++ nix/ci/test.nix
@@ -5,7 +5,6 @@ let
   findFlakeSrc = name: fetchGit {
     url = with lock.nodes.${name}.locked;"https://github.com/${owner}/${repo}";
     inherit (lock.nodes.${name}.locked) rev;
-    allRefs = true;
   };
 
   src = findFlakeSrc "nixpkgs";
