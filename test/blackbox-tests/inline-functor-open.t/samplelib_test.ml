6ef78ffe9c3108480de0530ba9f698ab8b0db604
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2023-07-21 21:18:03 -0700

    Update samplelib_test.ml

--- test/blackbox-tests/inline-functor-open.t/samplelib_test.ml
+++ test/blackbox-tests/inline-functor-open.t/samplelib_test.ml
@@ -1,7 +1,5 @@
 let test3 () =
   let open Samplelib.MonadOps (Samplelib.Promise) in
-  (* let module X = Samplelib.MonadOps (Samplelib.Promise) in *)
-  (* let open X in *)
   return 2
 
 let _ = test3 ()
