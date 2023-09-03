d9d0af9fec0620c100244679b0991a10b175f8c4
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2023-09-02 19:26:38 -0700

    feat: allow exporting invalid OCaml identifiers in JS

--- ppx/ast_attributes.ml
+++ ppx/ast_attributes.ml
@@ -418,6 +418,13 @@ let is_inline : attr -> bool =
 
 let has_inline_payload (attrs : t) = Ext_list.find_first attrs is_inline
 
+let is_mel_as : attr -> bool =
+ fun { attr_name = { txt; loc }; _ } ->
+  warn_if_bs ~loc txt;
+  txt = "mel.as" || txt = "bs.as" || txt = "as"
+
+let has_mel_as_payload (attrs : t) = Ext_list.find_first attrs is_mel_as
+
 (* We disable warning 61 in Melange externals since they're substantially
    different from OCaml externals. This warning doesn't make sense for a JS
    runtime *)
