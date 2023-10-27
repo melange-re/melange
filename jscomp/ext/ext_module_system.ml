4da7a3fd81473210f5601083a34221d3538affec
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2023-10-26 18:39:19 -0700

    fix: open `//`

--- jscomp/ext/ext_module_system.ml
+++ jscomp/ext/ext_module_system.ml
@@ -36,7 +36,10 @@ let compatible ~dep t =
 
 (* in runtime lib, [es6] and [es6] are treated the same way *)
 let runtime_dir = function NodeJS -> "js" | Es6 | Es6_global -> "es6"
-let runtime_package_path js_file = (Literals.package_name ^ ".js") // js_file
+
+let runtime_package_path =
+  let ( // ) = Ext_path.( // ) in
+  fun js_file -> (Literals.package_name ^ ".js") // js_file
 
 let to_string = function
   | NodeJS -> "commonjs"
