8e663d301948259c4bbb6c9b4c992b1be0255c40
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2023-11-02 16:05:29 -0700

    fix(test): don't set up include to Belt in blackbox tests

--- test/blackbox-tests/setup.sh
+++ test/blackbox-tests/setup.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-export MELANGELIB="$INSIDE_DUNE/jscomp/runtime/.js.objs/melange:$INSIDE_DUNE/jscomp/others/.belt.objs/melange:$INSIDE_DUNE/jscomp/stdlib/.stdlib.objs/melange"
+export MELANGELIB="$INSIDE_DUNE/jscomp/runtime/.js.objs/melange:$INSIDE_DUNE/jscomp/stdlib/.stdlib.objs/melange"
 
