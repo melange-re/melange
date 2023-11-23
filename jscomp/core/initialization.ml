b7c047ef5db981ceaf96de4242cdec16f550d66b
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2023-11-23 09:46:18 -0800

    chore: remove Outcome_printer_ns (#921)

--- jscomp/core/initialization.ml
+++ jscomp/core/initialization.ml
@@ -53,7 +53,6 @@ module Global = struct
     Clflags.strict_sequence := true;
     Clflags.strict_formats := true;
     (* Turn on [-no-alias-deps] by default -- double check *)
-    Oprint.out_ident := Outcome_printer_ns.out_ident;
     Builtin_attributes.check_bs_attributes_inclusion :=
       Record_attributes_check.check_mel_attributes_inclusion;
     Builtin_attributes.check_duplicated_labels :=
