30c7dd3d381b0a9a740be34d8b5b16d18db6566d
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2023-12-04 17:15:38 -0800

    fix(stdlib): enable more functions in bytes / buffer (#957)

--- jscomp/stdlib/buffer.mli
+++ jscomp/stdlib/buffer.mli
@@ -121,8 +121,6 @@ val truncate : t -> int -> unit
 val add_char : t -> char -> unit
 (** [add_char b c] appends the character [c] at the end of buffer [b]. *)
 
-#ifdef BS
-#else
 val add_utf_8_uchar : t -> Uchar.t -> unit
 (** [add_utf_8_uchar b u] appends the {{:https://tools.ietf.org/html/rfc3629}
     UTF-8} encoding of [u] at the end of buffer [b].
@@ -142,7 +140,6 @@ val add_utf_16be_uchar : t -> Uchar.t -> unit
     at the end of buffer [b].
 
     @since 4.06 *)
-#endif
 
 val add_string : t -> string -> unit
 (** [add_string b s] appends the string [s] at the end of buffer [b]. *)
