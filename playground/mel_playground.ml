b71d5c5096621380ba12a39f154c81645c930510
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2023-12-19 11:23:33 +0000

    refactor(playground): rename `Jsoo_common` module, improve bindings (#992)

--- playground/mel_playground.ml
+++ playground/mel_playground.ml
@@ -25,148 +25,100 @@
 open Melstd
 open Melangelib
 open Melange_compiler_libs
-module Js = Jsoo_common.Js
+module Js = Jsoo_runtime.Js
+module Melange_OCaml_version = Ppxlib_ast__.Versions.OCaml_501
 
 module Melange_ast = struct
   external to_ppxlib :
     Melange_compiler_libs.Parsetree.structure ->
-    Ppxlib_ast__.Versions.OCaml_501.Ast.Parsetree.structure = "%identity"
+    Melange_OCaml_version.Ast.Parsetree.structure = "%identity"
 
   external from_ppxlib :
-    Ppxlib_ast__.Versions.OCaml_501.Ast.Parsetree.structure ->
+    Melange_OCaml_version.Ast.Parsetree.structure ->
     Melange_compiler_libs.Parsetree.structure = "%identity"
 end
 
 let warnings_collected : Location.report list ref = ref []
 
-(* We need to overload the original warning printer to capture the warnings
-   and not let them go through default printer (which will end up in browser
-   console) *)
-let playground_reporter ~f ~mk (loc : Location.t) w : Location.report option =
-  match f w with
-  | `Inactive -> None
-  | `Active { Warnings.id; message; is_error; sub_locs } ->
-      let msg_of_str str ppf = Format.pp_print_string ppf str in
-      let kind = mk ~is_error id in
-      let main = { Location.loc; txt = msg_of_str message } in
-      let sub =
-        List.map
-          ~f:(fun (loc, sub_message) ->
-            { Location.loc; txt = msg_of_str sub_message })
-          sub_locs
-      in
-      warnings_collected := { Location.kind; main; sub } :: !warnings_collected;
-      None
-
-let playground_warning_reporter =
-  let mk ~is_error id =
-    if is_error then Location.Report_warning_as_error id else Report_warning id
-  in
-  playground_reporter ~f:Warnings.report ~mk
-
-let playground_alert_reporter =
-  let mk ~is_error id =
-    if is_error then Location.Report_alert_as_error id else Report_alert id
-  in
-  playground_reporter ~f:Warnings.report_alert ~mk
-
-let error_of_exn e =
-  match Location.error_of_exn e with
-  | Some (`Ok e) -> Some e
-  | Some `Already_displayed -> None
-  | None -> (
-      match Ocaml_common.Location.error_of_exn e with
-      | Some (`Ok e) -> Some e
-      | Some `Already_displayed | None -> None)
-
 module From_ppxlib =
-  Ppxlib_ast.Convert (Ppxlib_ast.Selected_ast) (Ppxlib_ast__.Versions.OCaml_501)
+  Ppxlib_ast.Convert (Ppxlib_ast.Selected_ast) (Melange_OCaml_version)
 
-module Printer = struct
-  let print_expr typ =
-    Printtyp.reset ();
-    Format.asprintf "%a" !Oprint.out_type (Printtyp.tree_of_typexp Type typ)
+(* Collects the type information from the typed_tree, so we can use that data
+   to display types on hover etc. *)
+let collect_type_hints =
+  let module Printer = struct
+    let print_expr typ =
+      Printtyp.reset ();
+      Format.asprintf "%a" !Oprint.out_type (Printtyp.tree_of_typexp Type typ)
 
-  let print_pattern pat =
-    Printtyp.reset ();
-    Format.asprintf "%a" !Oprint.out_type
-      (Printtyp.tree_of_typexp Type pat.Typedtree.pat_type)
+    let print_pattern pat =
+      Printtyp.reset ();
+      Format.asprintf "%a" !Oprint.out_type
+        (Printtyp.tree_of_typexp Type pat.Typedtree.pat_type)
 
-  let print_decl ~recStatus name decl =
-    Printtyp.reset ();
-    Format.asprintf "%a" !Oprint.out_sig_item
-      (Printtyp.tree_of_type_declaration (Ident.create_local name) decl
-         recStatus)
-end
-
-(* Collects the type information from the typed_tree, so we can use that
- * data to display types on hover etc. *)
-let collect_type_hints typed_tree =
-  let open Typedtree in
-  let create_type_hint_obj loc kind hint =
-    let open Location in
-    let _, startline, startcol = Location.get_pos_info loc.loc_start in
-    let _, endline, endcol = Location.get_pos_info loc.loc_end in
-    Js.(
-      obj
+    let print_decl ~recStatus name decl =
+      Printtyp.reset ();
+      Format.asprintf "%a" !Oprint.out_sig_item
+        (Printtyp.tree_of_type_declaration (Ident.create_local name) decl
+           recStatus)
+  end in
+  fun (structure, _) ->
+    let open Typedtree in
+    let create_type_hint_obj loc kind hint =
+      let open Location in
+      let _, startline, startcol = Location.get_pos_info loc.loc_start in
+      let _, endline, endcol = Location.get_pos_info loc.loc_end in
+      Js.obj
         [|
           ( "start",
-            obj
+            Js.obj
               [|
                 ("line", startline |> float_of_int |> Js.number_of_float);
                 ("col", startcol |> float_of_int |> Js.number_of_float);
               |] );
           ( "end",
-            obj
+            Js.obj
               [|
                 ("line", endline |> float_of_int |> Js.number_of_float);
                 ("col", endcol |> float_of_int |> Js.number_of_float);
               |] );
           ("kind", Js.string kind);
           ("hint", Js.string hint);
-        |])
-  in
-  let structure, _ = typed_tree in
-  let acc = ref [] in
-  let cur_rec_status = ref None in
-  let open Tast_iterator in
-  let expr_iter iter exp =
-    let hint = Printer.print_expr exp.exp_type in
-    let obj = create_type_hint_obj exp.exp_loc "expression" hint in
-    acc := obj :: !acc;
-    Tast_iterator.default_iterator.expr iter exp
-  in
-  let value_binding_iter iter binding =
-    let hint = Printer.print_expr binding.vb_expr.exp_type in
-    let obj = create_type_hint_obj binding.vb_loc "binding" hint in
-    acc := obj :: !acc;
-    Tast_iterator.default_iterator.value_binding iter binding
-  in
-  let core_type_iter iter ct =
-    let hint = Printer.print_expr ct.ctyp_type in
-    let obj = create_type_hint_obj ct.ctyp_loc "core_type" hint in
-    acc := obj :: !acc;
-    Tast_iterator.default_iterator.typ iter ct
-  in
-  let pattern_iter iter pat =
-    let hint = Printer.print_pattern pat in
-    let obj = create_type_hint_obj pat.pat_loc "pattern_type" hint in
-    acc := obj :: !acc;
-    Tast_iterator.default_iterator.pat iter pat
-  in
-  let type_declarations_iter iter ((rec_flag, _) as td) =
-    let status =
-      match rec_flag with
-      | Asttypes.Nonrecursive -> Types.Trec_not
-      | Recursive -> Trec_first
+        |]
     in
-    cur_rec_status := Some status;
-    Tast_iterator.default_iterator.type_declarations iter td
-  in
-  let type_declaration_iter iter tdecl =
-    let open Types in
-    let () =
-      match !cur_rec_status with
+    let acc = ref [] in
+    let cur_rec_status = ref None in
+    let open Tast_iterator in
+    let expr_iter iter exp =
+      let hint = Printer.print_expr exp.exp_type in
+      let obj = create_type_hint_obj exp.exp_loc "expression" hint in
+      acc := obj :: !acc;
+      Tast_iterator.default_iterator.expr iter exp
+    and value_binding_iter iter binding =
+      let hint = Printer.print_expr binding.vb_expr.exp_type in
+      let obj = create_type_hint_obj binding.vb_loc "binding" hint in
+      acc := obj :: !acc;
+      Tast_iterator.default_iterator.value_binding iter binding
+    and core_type_iter iter ct =
+      let hint = Printer.print_expr ct.ctyp_type in
+      let obj = create_type_hint_obj ct.ctyp_loc "core_type" hint in
+      acc := obj :: !acc;
+      Tast_iterator.default_iterator.typ iter ct
+    and pattern_iter iter pat =
+      let hint = Printer.print_pattern pat in
+      let obj = create_type_hint_obj pat.pat_loc "pattern_type" hint in
+      acc := obj :: !acc;
+      Tast_iterator.default_iterator.pat iter pat
+    and type_declarations_iter iter ((rec_flag, _) as td) =
+      let status =
+        match rec_flag with
+        | Asttypes.Nonrecursive -> Types.Trec_not
+        | Recursive -> Trec_first
+      in
+      cur_rec_status := Some status;
+      Tast_iterator.default_iterator.type_declarations iter td
+    and type_declaration_iter iter tdecl =
+      (match !cur_rec_status with
       | Some recStatus -> (
           let hint =
             Printer.print_decl ~recStatus tdecl.typ_name.Asttypes.txt
@@ -179,33 +131,38 @@ let collect_type_hints typed_tree =
           match recStatus with
           | Trec_not | Trec_first -> cur_rec_status := Some Trec_next
           | _ -> ())
-      | None -> ()
+      | None -> ());
+      Tast_iterator.default_iterator.type_declaration iter tdecl
     in
-    Tast_iterator.default_iterator.type_declaration iter tdecl
-  in
-  let iterator =
-    {
-      Tast_iterator.default_iterator with
-      expr = expr_iter;
-      value_binding = value_binding_iter;
-      typ = core_type_iter;
-      pat = pattern_iter;
-      type_declarations = type_declarations_iter;
-      type_declaration = type_declaration_iter;
-    }
-  in
-  iterator.structure iterator structure;
-  Js.array (!acc |> Array.of_list)
+    let iterator =
+      {
+        Tast_iterator.default_iterator with
+        expr = expr_iter;
+        value_binding = value_binding_iter;
+        typ = core_type_iter;
+        pat = pattern_iter;
+        type_declarations = type_declarations_iter;
+        type_declaration = type_declaration_iter;
+      }
+    in
+    iterator.structure iterator structure;
+    Js.array (!acc |> Array.of_list)
 
 let compile =
   let module To_ppxlib =
-    Ppxlib_ast.Convert
-      (Ppxlib_ast__.Versions.OCaml_501)
-      (Ppxlib_ast.Selected_ast)
+    Ppxlib_ast.Convert (Melange_OCaml_version) (Ppxlib_ast.Selected_ast)
   in
-  fun ~(impl :
-         Lexing.lexbuf ->
-         Ppxlib_ast__.Versions.OCaml_501.Ast.Parsetree.structure) str : Js.t ->
+  let error_of_exn e =
+    match Location.error_of_exn e with
+    | Some (`Ok e) -> Some e
+    | Some `Already_displayed -> None
+    | None -> (
+        match Ocaml_common.Location.error_of_exn e with
+        | Some (`Ok e) -> Some e
+        | Some `Already_displayed | None -> None)
+  in
+  fun ~(impl : Lexing.lexbuf -> Melange_OCaml_version.Ast.Parsetree.structure)
+      str ->
     let modulename = "Test" in
     (* let env = !Toploop.toplevel_env in *)
     (* let modulename = module_of_filename ppf sourcefile outputprefix in *)
@@ -240,31 +197,34 @@ let compile =
       in
       typed_tree |> Translmod.transl_implementation modulename
       |> (* Printlambda.lambda ppf *) fun { Lambda.code = lam; _ } ->
-      let buffer = Buffer.create 1000 in
-      let () =
-        Js_dump_program.pp_deps_program ~output_prefix:""
-          ~package_info:Js_packages_info.empty
-          ~output_info:
-            { Js_packages_info.module_system = Es6; suffix = Js_suffix.default }
-          (Js_pp.from_buffer buffer)
-          (Lam_compile_main.compile "" lam)
+      let v =
+        let buffer = Buffer.create 1000 in
+        let () =
+          Js_dump_program.pp_deps_program ~output_prefix:""
+            ~package_info:Js_packages_info.empty
+            ~output_info:
+              {
+                Js_packages_info.module_system = Es6;
+                suffix = Js_suffix.default;
+              }
+            (Js_pp.from_buffer buffer)
+            (Lam_compile_main.compile "" lam)
+        in
+        Buffer.contents buffer
       in
-      let v = Buffer.contents buffer in
       let type_hints = collect_type_hints typed_tree in
-      Js.(
-        obj
-          [|
-            ("js_code", Js.string v);
-            ( "warnings",
-              List.rev_map ~f:Jsoo_common.warning_error_to_js
-                !warnings_collected
-              |> Array.of_list |> Js.array );
-            ("type_hints", type_hints);
-          |])
+      Js.obj
+        [|
+          ("js_code", Js.string v);
+          ( "warnings",
+            List.rev_map ~f:Toolchain.warning_error_to_js !warnings_collected
+            |> Array.of_list |> Js.array );
+          ("type_hints", type_hints);
+        |]
       (* Format.fprintf output_ppf {| { "js_code" : %S }|} v ) *)
     with e -> (
       match error_of_exn e with
-      | Some error -> Jsoo_common.warning_error_to_js error
+      | Some error -> Toolchain.warning_error_to_js error
       | None -> (
           let default =
             lazy
@@ -281,7 +241,7 @@ let compile =
               | warnings ->
                   let type_ = "warning_errors" in
                   let jsErrors =
-                    List.rev_map ~f:Jsoo_common.warning_error_to_js warnings
+                    List.rev_map ~f:Toolchain.warning_error_to_js warnings
                     |> Array.of_list
                   in
                   Js.obj
@@ -291,9 +251,45 @@ let compile =
                     |])
           | _ -> Lazy.force default))
 
-let export (field : Js.t) v = Js.set (Js.pure_js_expr "globalThis") field v
-
 let () =
+  (* We need to overload the original warning printer to capture the warnings
+     and not let them go through default printer (which will end up in browser
+     console) *)
+  let playground_warning_reporter, playground_alert_reporter =
+    let playground_reporter ~f ~mk (loc : Location.t) w : Location.report option
+        =
+      match f w with
+      | `Inactive -> None
+      | `Active { Warnings.id; message; is_error; sub_locs } ->
+          let msg_of_str str ppf = Format.pp_print_string ppf str in
+          let kind = mk ~is_error id in
+          let main = { Location.loc; txt = msg_of_str message } in
+          let sub =
+            List.map
+              ~f:(fun (loc, sub_message) ->
+                { Location.loc; txt = msg_of_str sub_message })
+              sub_locs
+          in
+          warnings_collected :=
+            { Location.kind; main; sub } :: !warnings_collected;
+          None
+    in
+    let playground_warning_reporter =
+      let mk ~is_error id =
+        if is_error then Location.Report_warning_as_error id
+        else Report_warning id
+      in
+      playground_reporter ~f:Warnings.report ~mk
+    and playground_alert_reporter =
+      let mk ~is_error id =
+        if is_error then Location.Report_alert_as_error id else Report_alert id
+      in
+      playground_reporter ~f:Warnings.report_alert ~mk
+    in
+
+    (playground_warning_reporter, playground_alert_reporter)
+  in
+
   let load_cmi ~unit_name : Persistent_env.Persistent_signature.t option =
     match
       Initialization.find_in_path_exn
@@ -315,17 +311,16 @@ let () =
   Load_path.add_dir "/static"
 
 let () =
-  export (Js.string "ocaml")
+  Js.set
+    (Js.pure_js_expr "globalThis")
+    (Js.string "ocaml")
     (Js.obj
        [|
          ( "compileML",
            Js.wrap_meth_callback (fun _ code ->
                compile
                  ~impl:(fun
-                     buf
-                     :
-                     Ppxlib_ast__.Versions.OCaml_501.Ast.Parsetree.structure
-                   ->
+                     buf : Melange_OCaml_version.Ast.Parsetree.structure ->
                    Melange_ast.to_ppxlib
                      (Melange_compiler_libs.Parse.implementation buf))
                  (Js.to_string code)) );
@@ -333,24 +328,21 @@ let () =
            Js.wrap_meth_callback (fun _ code ->
                compile
                  ~impl:(fun
-                     buf
-                     :
-                     Ppxlib_ast__.Versions.OCaml_501.Ast.Parsetree.structure
-                   ->
+                     buf : Melange_OCaml_version.Ast.Parsetree.structure ->
                    From_ppxlib.copy_structure
                      (Reason_toolchain.RE.implementation buf))
                  (Js.to_string code)) );
          ("version", Js.string Melange_version.version);
          ( "parseRE",
            Js.wrap_meth_callback (fun _ re_string ->
-               Jsoo_common.Reason.parseRE re_string) );
+               Toolchain.parseRE re_string) );
          ( "parseML",
            Js.wrap_meth_callback (fun _ ocaml_string ->
-               Jsoo_common.Reason.parseML ocaml_string) );
+               Toolchain.parseML ocaml_string) );
          ( "printRE",
            Js.wrap_meth_callback (fun _ reason_ast_and_comments ->
-               Jsoo_common.Reason.printRE reason_ast_and_comments) );
+               Toolchain.printRE reason_ast_and_comments) );
          ( "printML",
            Js.wrap_meth_callback (fun _ ocaml_ast_and_comments ->
-               Jsoo_common.Reason.printML ocaml_ast_and_comments) );
+               Toolchain.printML ocaml_ast_and_comments) );
        |])
