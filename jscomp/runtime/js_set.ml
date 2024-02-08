1d510a96cf697571cff795980dec5c7560931e29
Corentin Leruth <corentin.leruth@gmail.com>
2024-02-08 16:44:40 +0100

    improve js_set bindings

--- jscomp/runtime/js_set.ml
+++ jscomp/runtime/js_set.ml
@@ -3,10 +3,12 @@
 type 'a t
 
 external make : unit -> 'a t = "Set" [@@mel.new]
+external fromArray : 'a array -> 'a t = "Set" [@@mel.new]
+external toArray : 'a t -> 'a array = "Array.from"
 external size : 'a t -> int = "size" [@@mel.get]
 external add : value:'a -> 'a t = "add" [@@mel.send.pipe: 'a t]
-external clear : 'a t -> unit = "add" [@@mel.send]
-external delete : value:'a t -> bool = "delete" [@@mel.send]
+external clear : 'a t -> unit = "clear" [@@mel.send]
+external delete : value:'a -> bool = "delete" [@@mel.send.pipe: 'a t]
 external difference : other:'a t -> 'a t = "difference" [@@mel.send.pipe: 'a t]
 
 (* commented out until Melange has a plan for iterators
---
+++ jscomp/test/js_set_test.ml
@@ -0,0 +1,38 @@
+let suites = Mt.[
+    "fromArray/toArray", (fun _ ->
+      let set = Js.Set.fromArray [|1;2;3|] in
+      Eq([|1;2;3;|], Js.Set.toArray set)
+    );
+    "size - empty set", (fun _ ->
+      let set = Js.Set.make () in
+      Eq(0, Js.Set.size set)
+    );
+    "size", (fun _ ->
+      let set = Js.Set.fromArray [|"one"; "two"|] in
+      Eq(2, Js.Set.size set)
+    );
+    "size - with duplicates", (fun _ ->
+      let set = Js.Set.fromArray [|"one"; "two"|] in
+      Eq(1, Js.Set.size set)
+    );
+    "has - true", (fun _ ->
+      let set = Js.Set.fromArray [|"one"; "two"|] in
+      Eq(true, Js.Set.has set ~value:"two")
+    );
+    "has - false", (fun _ ->
+      let set = Js.Set.fromArray [|"one"; "two"|] in
+      Eq(false, Js.Set.has set ~value:"three")
+    );
+    "delete", (fun _ ->
+      let set = Js.Set.fromArray [|"one"; "two"|] in
+      let deleted = Js.Set.delete set ~value:"two" in
+      Eq((true, false), (deleted, Js.Set.has set ~value:"two"))
+    );
+    "difference", (fun _ ->
+      let odds = Js.Set.fromArray [|1; 3; 5; 7; 9|] in
+      let squares = Js.Set.fromArray [|1; 4; 9|] in
+      Eq([|3; 5; 7|], Js.Set.difference odds ~other:squares |> Js.Set.toArray)
+    );
+]
+
+;; Mt.from_pair_suites __MODULE__ suites
