7c5755b7489549d8b25d7a2de0183bb8cf31e5e8
Corentin Leruth <corentin.leruth@gmail.com>
2024-02-12 12:14:43 +0100

    add label

--- jscomp/runtime/js_iterator.ml
+++ jscomp/runtime/js_iterator.ml
@@ -3,4 +3,4 @@ type 'a value = { done_ : bool; value : 'a option }
 
 external next : 'a t -> 'a value = "next" [@@mel.send]
 external toArray : 'a t -> 'a array = "Array.from"
-external toArrayWithMapper : 'a t -> ('a -> 'b) -> 'b array = "Array.from"
+external toArrayWithMapper : 'a t -> f:('a -> 'b) -> 'b array = "Array.from"
