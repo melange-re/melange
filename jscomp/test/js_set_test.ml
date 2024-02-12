b1552502434dea99eb18dfe66d57c1ac79c91135
Corentin Leruth <corentin.leruth@gmail.com>
2024-02-12 14:27:50 +0100

    Js_set: remove newer functions + add more tests

--- jscomp/test/js_set_test.ml
+++ jscomp/test/js_set_test.ml
@@ -8,7 +8,7 @@ let suites = Mt.[
       Eq(0, Js.Set.size set)
     );
     "size", (fun _ ->
-      let set = Js.Set.fromArray [|"one"; "two"|] in
+      let set = Js.Set.fromArray [|"one"; "two"; "two"|] in
       Eq(2, Js.Set.size set)
     );
     "size - with duplicates", (fun _ ->
@@ -28,10 +28,31 @@ let suites = Mt.[
       let deleted = Js.Set.delete set ~value:"two" in
       Eq((true, false), (deleted, Js.Set.has set ~value:"two"))
     );
-    "difference", (fun _ ->
-      let odds = Js.Set.fromArray [|1; 3; 5; 7; 9|] in
-      let squares = Js.Set.fromArray [|1; 4; 9|] in
-      Eq([|3; 5; 7|], Js.Set.difference odds ~other:squares |> Js.Set.toArray)
+    "add", (fun _ ->
+      let set = Js.Set.(make () |> add ~value:"one" |> add ~value:"two" ) in
+      Eq([|"one"; "two"|], Js.Set.toArray set)
+    );
+    "clear", (fun _ ->
+      let set = Js.Set.fromArray [|"one"; "two"|] in
+      Js.Set.clear set;
+      Eq(0, Js.Set.size set)
+    );
+    "add mutate + return new set", (fun _ ->
+      (* demonstrate that [add] returns a new set + mutates the original one. *)
+      let set_1 = Js.Set.make () in
+      let set_2 = Js.Set.add ~value:"one" set_1 in
+      let set_3 = Js.Set.add ~value:"two" set_2 in
+      let all_same_size = 
+        let size = 2 in
+        Js.Set.size set_1 = size && Js.Set.size set_2 = size && Js.Set.size set_3 = size in
+      let all_same_ref = set_1 == set_2 && set_2 == set_3 in
+      Eq((true, true), (all_same_size, all_same_ref))
+    );
+    "forEach", (fun _ ->
+      let set = Js.Set.fromArray [|"one"; "two"|] in
+      let arr = ref([||]) in
+      set |> Js.Set.forEach ~f:(fun value -> let (_i: int) = Js.Array.push !arr ~value in ());
+      Eq([|"one"; "two"|], !arr)
     );
 ]
 
