2030e54143f8e3e8d29e988599c09521e6186dc4
Corentin Leruth <corentin.leruth@gmail.com>
2024-02-12 12:15:37 +0100

    fix field name

--- jscomp/runtime/js_iterator.ml
+++ jscomp/runtime/js_iterator.ml
@@ -1,5 +1,9 @@
 type 'a t
-type 'a value = { done_ : bool Js_nullable.t; value : 'a Js_nullable.t }
+
+type 'a value = {
+  done_ : bool Js_nullable.t; [@mel.as "done"]
+  value : 'a Js_nullable.t;
+}
 
 external next : 'a t -> 'a value = "next" [@@mel.send]
 external toArray : 'a t -> 'a array = "Array.from"
