2e76ef49ed6ecd0add8d204ac5e9e9660f47adc1
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2024-03-14 14:54:18 -0700

    wip merged submodule

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
     melange-compiler-libs = {
       # this changes rarely, and it's better than having to rely on nix's poor
       # support for submodules
-      url = "github:melange-re/melange-compiler-libs/anmonteiro/warning-51";
+      url = "github:melange-re/melange-compiler-libs";
       inputs.flake-utils.follows = "flake-utils";
       inputs.nixpkgs.follows = "nixpkgs";
     };
