f6e4491999935132357b3e6d21fbc9380aa9728d
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2024-11-18 01:07:48 -0800

    nix ci: add prewarm step to separate melange-only build output to another step (#1230)
    * nix ci: add prewarm step to separate melange-only build output to
    another step
    
    * ubuntoo
    
    * s/command/run
    
    * nix build shell
    
    * fmt, don't do it on macOS?

--- nix/ci/test.nix
+++ nix/ci/test.nix
@@ -1,39 +1,13 @@
 { ocamlVersion }:
 
 let
-  lock = builtins.fromJSON (builtins.readFile ./../../flake.lock);
-  findFlakeSrc = { name, allRefs ? false }: fetchGit {
-    url = with lock.nodes.${name}.locked;"https://github.com/${owner}/${repo}";
-    inherit (lock.nodes.${name}.locked) rev;
-    inherit allRefs;
-  };
-
-  src = findFlakeSrc { name = "nixpkgs"; };
-  nix-filter-src = findFlakeSrc { name = "nix-filter"; };
-  melange-compiler-libs-src = findFlakeSrc {
-    name = "melange-compiler-libs";
-    allRefs = true;
-  };
-  nix-filter = import "${nix-filter-src}";
-
-  pkgs = import src {
-    extraOverlays = [
-      (self: super: {
-        ocamlPackages = super.ocaml-ng."ocamlPackages_${ocamlVersion}";
-      })
-    ];
-  };
-  inherit (pkgs) stdenv nodejs yarn git lib nodePackages ocamlPackages tree;
-  packages = rec {
-    melange = pkgs.callPackage ./.. {
-      inherit nix-filter;
-      melange-compiler-libs-vendor-dir = melange-compiler-libs-src;
-    };
-  };
+  src = import ./sources.nix { inherit ocamlVersion; };
+  inherit (src) pkgs packages;
   inputString =
     builtins.substring
       11 32
       (builtins.unsafeDiscardStringContext packages.melange.outPath);
+  inherit (pkgs) stdenv nodejs yarn git lib nodePackages ocamlPackages tree;
 in
 
 with ocamlPackages;
