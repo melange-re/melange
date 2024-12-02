5e6cdac08bd3fc4031cf7f64b47f1729322bb529
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2024-12-01 19:16:16 -0800

    more docs
    improve blob / primitives distinction
    
    dom_formData insetad of dom_formdata
    
    Use blob and file in FormData
    
    Move from dom to js
    
    fix some bindings

---
+++ jscomp/runtime/js_blob.ml
@@ -0,0 +1,54 @@
+(* Copyright (C) 2022- Authors of Melange
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * In addition to the permissions granted to you by the LGPL, you may combine
+ * or link a "work that uses the Library" with a publicly distributed version
+ * of this file to produce a combined library or application, then distribute
+ * that combined work under the terms of your choosing, with no requirement
+ * to comply with the obligations normally placed on you by section 4 of the
+ * LGPL version 3 (or the corresponding section of a later version of the LGPL
+ * should you choose to use a later version).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. *)
+
+(* https://developer.mozilla.org/en-US/docs/Web/API/Blob/Blob *)
+
+type t
+
+external make : string Js.iterator -> filename:string -> t = "Blob"
+[@@mel.new]
+(** [make contents_array ~filename] creates a new file from an iterable object
+  such as an Array, having ArrayBuffers, TypedArrays, DataViews, Blobs,
+  strings, or a mix of any of such elements, that will be put inside the File.
+  Note that strings here are encoded as UTF-8, unlike the usual JavaScript
+  UTF-16 strings. *)
+
+external arrayBuffer : t -> Js.Typed_array.ArrayBuffer.t Js.promise
+  = "arrayBuffer"
+[@@mel.send]
+
+external bytes : t -> Js.Typed_array.Uint8Array.t Js.promise = "bytes"
+[@@mel.send]
+
+external size : t -> float = "size" [@@mel.get]
+
+external slice : ?start:int -> ?end_:int -> ?contentType:string -> t = "slice"
+[@@mel.send.pipe: t]
+
+external text : t -> string Js.promise = "text" [@@mel.send]
+external type_ : t -> string = "type" [@@mel.get]
+
+(* XXX: stream can't be bound until we have some bindings to ReadableStream *)
+(* https://developer.mozilla.org/en-US/docs/Web/API/ReadableStream *)
+(* external stream : t -> ReadableStream.t = "stream" *)
