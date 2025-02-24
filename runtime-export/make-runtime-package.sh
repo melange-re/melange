0ffd83ecbfa0e251d0a14bc33f514f24dd796c63
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2025-02-23 21:18:27 -0800

    runtime export: generate README.md and dependencies per package (#1341)
    * runtime export: generate README.md and dependencies per package
    
    * add readme template

--- runtime-export/make-runtime-package.sh
+++ runtime-export/make-runtime-package.sh
@@ -2,10 +2,39 @@
 
 name=$1
 src=$2
-target=$3
+readme_src=$3
+target=$4
 
-jq '.name = $name | .version = $version' \
-  --arg name "$name" \
-  --arg version "$MELANGE_RUNTIME_VERSION" \
-  $src > $target
+add_dependencies() {
+  local JQ_BASE='.name = $name | .version = $version'
+  case "$name" in
+    "melange.js")
+      # melange.js has no dependencies
+      jq "$JQ_BASE" \
+        --arg name "$name" \
+        --arg version "$MELANGE_RUNTIME_VERSION"
+      ;;
+    "melange")
+      # melange depends on melange.js
+      jq "$JQ_BASE | .dependencies.\"melange.js\" = \$version" \
+        --arg name "$name" \
+        --arg version "$MELANGE_RUNTIME_VERSION"
+      ;;
+    "melange.belt")
+      # melange.belt depends on melange.js and melange
+      jq "$JQ_BASE | .dependencies.\"melange.js\" = \$version | .dependencies.\"melange\" = \$version" \
+        --arg name "$name" \
+        --arg version "$MELANGE_RUNTIME_VERSION"
+      ;;
+    *)
+      echo "Unknown library: $name" >&2
+      exit 1
+      ;;
+  esac
+}
 
+# Generate package.json with deps
+add_dependencies < "$src" > "$target"
+
+# Generate README.md for the package
+sed "s/%{library_name}/$name/g; s/%{version}/$MELANGE_RUNTIME_VERSION/g" "$readme_src" > "README.md"
