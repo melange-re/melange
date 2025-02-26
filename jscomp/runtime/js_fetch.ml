ef0faec2115168a8215e604d5882a6d4fb5e70ac
David Sancho <dsnxmoreno@gmail.com>
2025-02-26 10:03:13 +0100

    Use right year for copyright in js_fetch
    Co-authored-by: Antonio Nuno Monteiro <anmonteiro@gmail.com>

--- jscomp/runtime/js_fetch.ml
+++ jscomp/runtime/js_fetch.ml
@@ -1,4 +1,4 @@
-(* Copyright (C) 2022- Authors of Melange
+(* Copyright (C) 2025- Authors of Melange
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
