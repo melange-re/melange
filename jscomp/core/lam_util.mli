62e640eabf6060f49460d6bbd1d17c33ffe9e9c3
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2025-04-05 13:44:04 -0700

    refactor: remove dead code in Lam_util (#1401)

--- jscomp/core/lam_util.mli
+++ jscomp/core/lam_util.mli
@@ -31,7 +31,7 @@ val field_flatten_get :
   Ident.t ->
   int ->
   Lambda.field_dbg_info ->
-  Lam_stats.ident_tbl ->
+  Lam_id_kind.t Ident.Hash.t ->
   Lam.t
 (** [field_flatten_get cb v i tbl]
     try to remove the indirection of [v.(i)] by inlining when [v]
@@ -55,6 +55,4 @@ val alias_ident_or_global :
   Lam_stats.t -> Ident.t -> Ident.t -> Lam_id_kind.t -> unit
 
 val refine_let : kind:Lam_group.let_kind -> Ident.t -> Lam.t -> Lam.t -> Lam.t
-val generate_label : ?name:string -> unit -> string
 val not_function : Lam.t -> bool
-val is_function : Lam.t -> bool
