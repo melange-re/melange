056679c4658251c161159fa49a80b5cbfc52d2a6
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2025-04-06 12:53:06 -0700

    js_parser: prune `ppx.context`

--- jscomp/js_parser/flow_lexer.mli
+++ jscomp/js_parser/flow_lexer.mli
@@ -1,21 +1,3 @@
-[@@@ocaml.ppx.context
-  {
-    tool_name = "ppx_driver";
-    include_dirs = [];
-    hidden_include_dirs = [];
-    load_path = ([], []);
-    open_modules = [];
-    for_package = None;
-    debug = false;
-    use_threads = false;
-    use_vmthreads = false;
-    recursive_types = false;
-    principal = false;
-    transparent_modules = false;
-    unboxed_types = false;
-    unsafe_string = false;
-    cookies = [("library-name", "flow_parser")]
-  }]
 val jsx_child : Lex_env.t -> (Lex_env.t * Lex_result.t)
 val regexp : Lex_env.t -> (Lex_env.t * Lex_result.t)
 val jsx_tag : Lex_env.t -> (Lex_env.t * Lex_result.t)
