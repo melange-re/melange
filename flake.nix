434b2dc0d3c6e242ae9b7e9bf8de2e72b62842c1
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2025-06-15 16:32:11 -0700

    wip

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
     melange-compiler-libs = {
       # this changes rarely, and it's better than having to rely on nix's poor
       # support for submodules
-      url = "github:melange-re/melange-compiler-libs";
+      url = "github:melange-re/melange-compiler-libs/5.3";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
