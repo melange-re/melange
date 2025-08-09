d8fa8c06394e785378d4354d371564d3e804df72
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2025-08-09 15:27:19 -0700

    core: move `Lam_pass_count` implementation to `Lam_pass_lets_dce`

--- jscomp/core/lam_pass_lets_dce.cppo.ml
+++ jscomp/core/lam_pass_lets_dce.cppo.ml
@@ -13,13 +13,216 @@
 
 open Import
 
+module Count = struct
+  (* A naive dead code elimination *)
+  type used_info = {
+    mutable times : int;
+    mutable captured : bool;
+        (* captured in functon or loop,
+           inline in such cases should be careful
+           1. can not inline mutable values
+           2. avoid re-computation
+        *)
+  }
+
+  type t = used_info Ident.Hashtbl.t
+  (* First pass: count the occurrences of all let-bound identifiers *)
+
+  type local_tbl = used_info Ident.Map.t
+
+  let dummy_info () = { times = 0; captured = false }
+  (* y is untouched *)
+
+  let absorb_info (x : used_info) (y : used_info) =
+    match (x, y) with
+    | { times = x0; _ }, { times = y0; captured } ->
+        x.times <- x0 + y0;
+        if captured then x.captured <- true
+
+  let pp_info (x : used_info) = Pp.textf "(<captured:%b>:%d)" x.captured x.times
+
+  let pp_occ_tbl =
+    let to_list h f =
+      Ident.Hashtbl.fold h ~init:[] ~f:(fun ~key ~data acc -> f key data :: acc)
+    in
+    fun tbl ->
+      to_list tbl (fun k v ->
+          Pp.box
+            (Pp.concat ~sep:Pp.space
+               [ Pp.text (Format.asprintf "%a" Ident.print k); pp_info v ]))
+      |> Pp.concat ~sep:Pp.newline
+
+  (* The global table [occ] associates to each let-bound identifier
+     the number of its uses (as a reference):
+     - 0 if never used
+     - 1 if used exactly once in and not under a lambda or within a loop
+         - when under a lambda,
+         - it's probably a closure
+         - within a loop
+         - update reference,
+         niether is good for inlining
+     - > 1 if used several times or under a lambda or within a loop.
+     The local table [bv] associates to each locally-let-bound variable
+     its reference count, as above.  [bv] is enriched at let bindings
+     but emptied when crossing lambdas and loops. *)
+  let collect_occurs lam : t =
+    let occ : t = Ident.Hashtbl.create 83 in
+
+    (* Current use count of a variable. *)
+    let used v =
+      match Ident.Hashtbl.find occ v with
+      | exception Not_found -> false
+      | { times; _ } -> times > 0
+    in
+
+    (* Entering a [let].  Returns updated [bv]. *)
+    let bind_var bv ident =
+      let r = dummy_info () in
+      Ident.Hashtbl.add occ ~key:ident ~data:r;
+      Ident.Map.add ~key:ident ~data:r bv
+    in
+
+    (* Record a use of a variable *)
+    let add_one_use bv ident =
+      match Ident.Map.find ident bv with
+      | r -> r.times <- r.times + 1
+      | exception Not_found -> (
+          (* ident is not locally bound, therefore this is a use under a lambda
+             or within a loop.  Increase use count by 2 -- enough so
+             that single-use optimizations will not apply. *)
+          match Ident.Hashtbl.find occ ident with
+          | r -> absorb_info r { times = 1; captured = true }
+          | exception Not_found ->
+              (* Not a let-bound variable, ignore *)
+              ())
+    in
+
+    let inherit_use bv ident bid =
+      let n =
+        match Ident.Hashtbl.find occ bid with
+        | exception Not_found -> dummy_info ()
+        | v -> v
+      in
+      match Ident.Map.find ident bv with
+      | r -> absorb_info r n
+      | exception Not_found -> (
+          (* ident is not locally bound, therefore this is a use under a lambda
+             or within a loop.  Increase use count by 2 -- enough so
+             that single-use optimizations will not apply. *)
+          match Ident.Hashtbl.find occ ident with
+          | r -> absorb_info r { n with captured = true }
+          | exception Not_found ->
+              (* Not a let-bound variable, ignore *)
+              ())
+    in
+
+    let rec count (bv : local_tbl) (lam : Lam.t) =
+      match lam with
+      | Lfunction { body = l; _ } -> count Ident.Map.empty l
+      (* when entering a function local [bv]
+          is cleaned up, so that all closure variables will not be
+          carried over, since the parameters are never rebound,
+          so it is fine to kep it empty
+      *)
+      | Lfor (_, l1, l2, _dir, l3) ->
+          count bv l1;
+          count bv l2;
+          count Ident.Map.empty l3
+      | Lwhile (l1, l2) ->
+          count Ident.Map.empty l1;
+          count Ident.Map.empty l2
+      | Lvar v | Lmutvar v -> add_one_use bv v
+      | Llet (_, v, Lvar w, l2) ->
+          (* v will be replaced by w in l2, so each occurrence of v in l2
+             increases w's refcount *)
+          count (bind_var bv v) l2;
+          inherit_use bv w v
+      | Llet (kind, v, l1, l2) ->
+          count (bind_var bv v) l2;
+          (* count [l2] first,
+             If v is unused, l1 will be removed, so don't count its variables *)
+          if kind = Strict || used v then count bv l1
+      | Lmutlet (_, l1, l2) ->
+          count bv l1;
+          count bv l2
+      | Lassign (_, l) ->
+          (* Lalias-bound variables are never assigned, so don't increase
+             this ident's refcount *)
+          count bv l
+      | Lglobal_module _ -> ()
+      | Lprim { args; _ } -> List.iter ~f:(count bv) args
+      | Lletrec (bindings, body) ->
+          List.iter ~f:(fun (_v, l) -> count bv l) bindings;
+          count bv body
+          (* Note there is a difference here when do beta reduction for *)
+      | Lapply { ap_func = Lfunction { params; body; _ }; ap_args = args; _ }
+        when List.same_length params args ->
+          count bv (Lam_beta_reduce.no_names_beta_reduce params body args)
+      (* | Lapply{fn = Lfunction{function_kind = Tupled; params; body}; *)
+      (*          args = [Lprim {primitive = Pmakeblock _;  args; _}]; _} *)
+      (*   when  List.same_length params  args -> *)
+      (*   count bv (Lam_beta_reduce.beta_reduce   params body args) *)
+      | Lapply { ap_func = l1; ap_args = ll; _ } ->
+          count bv l1;
+          List.iter ~f:(count bv) ll
+      | Lconst _cst -> ()
+      | Lswitch (l, sw) ->
+          count_default bv sw;
+          count bv l;
+          List.iter ~f:(fun (_, l) -> count bv l) sw.sw_consts;
+          List.iter ~f:(fun (_, l) -> count bv l) sw.sw_blocks
+      | Lstringswitch (l, sw, d) -> (
+          count bv l;
+          List.iter ~f:(fun (_, l) -> count bv l) sw;
+          match d with Some d -> count bv d | None -> ())
+      (* x2 for native backend *)
+      (* begin match sw with *)
+      (* | []|[_] -> count bv d *)
+      (* | _ -> count bv d ; count bv d *)
+      (* end *)
+      | Lstaticraise (_i, ls) -> List.iter ~f:(count bv) ls
+      | Lstaticcatch (l1, (_i, _), l2) ->
+          count bv l1;
+          count bv l2
+      | Ltrywith (l1, _v, l2) ->
+          count bv l1;
+          count bv l2
+      | Lifthenelse (l1, l2, l3) ->
+          count bv l1;
+          count bv l2;
+          count bv l3
+      | Lsequence (l1, l2) ->
+          count bv l1;
+          count bv l2
+      | Lsend (_, m, o, ll, _) ->
+          count bv m;
+          count bv o;
+          List.iter ~f:(count bv) ll
+      | Lifused (v, l) -> if used v then count bv l
+    and count_default bv sw =
+      match sw.sw_failaction with
+      | None -> ()
+      | Some al ->
+          if (not sw.sw_consts_full) && not sw.sw_blocks_full then (
+            (* default action will occur twice in native code *)
+            count bv al;
+            count bv al)
+          else (
+            (* default action will occur once *)
+            assert ((not sw.sw_consts_full) || not sw.sw_blocks_full);
+            count bv al)
+    in
+    count Ident.Map.empty lam;
+    occ
+end
+
 let find_string string_table lam =
   match lam with
   | Lam.Lconst (Const_string { s = ls; unicode = false }) -> ls
   | Lvar i -> Ident.Hashtbl.find string_table i
   | _ -> raise_notrace Not_found
 
-let lets_helper (count_var : Ident.t -> Lam_pass_count.used_info) lam : Lam.t =
+let lets_helper (count_var : Ident.t -> Count.used_info) lam : Lam.t =
   let subst : Lam.t Ident.Hashtbl.t = Ident.Hashtbl.create 32 in
   let string_table : string Ident.Hashtbl.t = Ident.Hashtbl.create 32 in
   let used v = (count_var v).times > 0 in
@@ -240,17 +443,15 @@ let lets_helper (count_var : Ident.t -> Lam_pass_count.used_info) lam : Lam.t =
   simplif lam
 
 (* To transform let-bound references into variables *)
-let apply_lets occ lambda =
-  let count_var v =
+let simplify_lets =
+  let count_var occ v =
     match Ident.Hashtbl.find occ v with
-    | exception Not_found -> Lam_pass_count.dummy_info ()
+    | exception Not_found -> Count.dummy_info ()
     | v -> v
   in
-  lets_helper count_var lambda
-
-let simplify_lets (lam : Lam.t) : Lam.t =
-  let occ = Lam_pass_count.collect_occurs lam in
+  fun (lam : Lam.t) : Lam.t ->
+    let occ = Count.collect_occurs lam in
 #ifndef MELANGE_RELEASE_BUILD
-  Log.warn ~loc:(Loc.of_pos __POS__) (Lam_pass_count.pp_occ_tbl occ);
+    Log.warn ~loc:(Loc.of_pos __POS__) (Count.pp_occ_tbl occ);
 #endif
-  apply_lets occ lam
+    lets_helper (count_var occ) lam
