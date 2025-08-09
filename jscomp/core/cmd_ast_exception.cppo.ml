0d3932a228128e47f65f8ea6a8c83b37d7b4d52a
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2025-08-09 15:38:20 -0700

    core: tighten bindings in `Cmd_ast_exception`

--- jscomp/core/cmd_ast_exception.cppo.ml
+++ jscomp/core/cmd_ast_exception.cppo.ml
@@ -2,17 +2,17 @@ type error = CannotRun of string | WrongMagic of string
 
 exception Error of error
 
-let report_error ppf = function
-  | CannotRun cmd ->
-      Format.fprintf ppf
-        "Error while running external preprocessor@.Command line: %s@." cmd
-  | WrongMagic cmd ->
-      Format.fprintf ppf
-        "External preprocessor does not produce a valid file@.Command line: \
-         %s@."
-        cmd
-
 let () =
+  let report_error ppf = function
+    | CannotRun cmd ->
+        Format.fprintf ppf
+          "Error while running external preprocessor@.Command line: %s@." cmd
+    | WrongMagic cmd ->
+        Format.fprintf ppf
+          "External preprocessor does not produce a valid file@.Command line: \
+           %s@."
+          cmd
+  in
   Location.register_error_of_exn (function
     | Error err ->
 #if OCAML_VERSION >= (5, 3, 0)
