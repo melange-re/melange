42dcaf44f74e5badd7ef9bad4a65e4aa176afbbe
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2025-09-01 01:07:03 -0700

    build: update flakes / remove overrides after 5.4 upstream update (#1586)

--- flake.nix
+++ flake.nix
@@ -16,31 +16,7 @@
       forAllSystems = f: nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed (system:
         let
           pkgs = nixpkgs.legacyPackages.${system}.extend (self: super: {
-            ocamlPackages = super.ocaml-ng.ocamlPackages_5_4.overrideScope (oself: osuper: {
-              reason = osuper.reason.overrideAttrs (o: {
-                src = builtins.fetchurl {
-                  url = "https://github.com/reasonml/reason/releases/download/3.17.0/reason-3.17.0.tbz";
-                  sha256 = "1sx5z269sry2xbca3d9sw7mh9ag773k02r9cgrz5n8gxx6f83j42";
-                };
-                propagatedBuildInputs = o.propagatedBuildInputs ++ [ oself.cmdliner ];
-                patches = [ ];
-                doCheck = false;
-              });
-              reason-react-ppx = osuper.reason-react-ppx.overrideAttrs (_: {
-                src = super.fetchFromGitHub {
-                  owner = "reasonml";
-                  repo = "reason-react";
-                  rev = "8454c63ee56afc7e7dce439eaff395aba0577d68";
-                  hash = "sha256-8Wkk6Aav1zk9me1UGz8D3z3bDdIc/MDt9E4YNS6zK1U=";
-                };
-              });
-              sedlex = osuper.sedlex.overrideAttrs (o: {
-                doCheck = false;
-              });
-              pp = osuper.pp.overrideAttrs (o: {
-                doCheck = false;
-              });
-            });
+            ocamlPackages = super.ocaml-ng.ocamlPackages_5_4;
           });
         in
         f pkgs);
