662c1e4320596e0c3a9b67ec6eaa2b81b89c5a28
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2025-11-08 16:03:59 -0800

    melstd: ignore warning 32 on compatibility hash code

--- jscomp/melstd/string.ml
+++ jscomp/melstd/string.ml
@@ -23,7 +23,7 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. *)
 
 module T = struct
-  let hash = Hashtbl.hash
+  let[@ocaml.warning "-32"] hash = Hashtbl.hash
 
   include StdLabels.String
 end
