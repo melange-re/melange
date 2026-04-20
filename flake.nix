959c8cde5a0135ccdaf2e5cc720cd6bce5ccd079
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2026-04-19 22:52:47 -0700

    build: reconcile flake overrides with upstream updates (#1743)

--- flake.nix
+++ flake.nix
@@ -25,35 +25,7 @@
           let
             pkgs = nixpkgs.legacyPackages.${system}.extend (
               self: super: {
-                ocamlPackages = super.ocaml-ng.ocamlPackages_5_5.overrideScope (
-                  oself: osuper: {
-                    dune_3 = osuper.dune_3.overrideAttrs (_: {
-                      # https://github.com/ocaml/dune/pull/14018
-                      src = super.fetchFromGitHub {
-                        owner = "ocaml";
-                        repo = "dune";
-                        rev = "9e80abbfb0b2ae09ff5b5412d419f3828992d13c";
-                        hash = "sha256-2ijxaxykiHga+cuOQJ+FtzDCw1b1xGoYPBUPQl/ujBc=";
-                      };
-                    });
-                    js_of_ocaml-compiler = osuper.js_of_ocaml-compiler.overrideAttrs (_: {
-                      src = super.fetchFromGitHub {
-                        owner = "ocsigen";
-                        repo = "js_of_ocaml";
-                        rev = "8df204bc6982ae2e1d20f07f02af8c5b0ea7aff5";
-                        hash = "sha256-Z30NLi3akYvLl+YardOBY8tkMqzSbsj3ime+ajlVqPM=";
-                      };
-                    });
-                    ocaml-lsp = osuper.ocaml-lsp.overrideAttrs (o: {
-                      postPatch = ''
-                        ${o.postPatch or ""}
-                        substituteInPlace ocaml-lsp-server/src/dune.ml \
-                          --replace-fail '| s -> Ok (`Mtime s.st_mtime)' \
-                                         '| s -> Ok (`Mtime (Stdune.Time.of_epoch_secs s.st_mtime))'
-                      '';
-                    });
-                  }
-                );
+                ocamlPackages = super.ocaml-ng.ocamlPackages_5_5;
               }
             );
           in
