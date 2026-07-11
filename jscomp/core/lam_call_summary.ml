99e93769241b6ede607110b3a4123bb1662d06c1
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2026-07-11 16:29:58 -0700

    fix: avoid relocating relative mel.module externals (#1800)

--- jscomp/core/lam_call_summary.ml
+++ jscomp/core/lam_call_summary.ml
@@ -8,13 +8,14 @@ type t =
       id : Ident.t;
       name : string;
       arity : Lam_arity.t;
+      relocatable : bool;
     }
 
 let print fmt = function
   | Unknown -> Format.fprintf fmt "Unknown"
   | Direct_primitive primitive ->
       Format.fprintf fmt "Direct(%a)" Lam_print.primitive primitive
-  | Direct_external { dynamic_import; id; name; arity = _ } ->
+  | Direct_external { dynamic_import; id; name; arity = _; relocatable = _ } ->
       Format.fprintf fmt "Direct(%s%s.%s)"
         (if dynamic_import then "import " else "")
         (Ident.name id) name
@@ -23,12 +24,17 @@ let is_unknown = function
   | Unknown -> true
   | Direct_primitive _ | Direct_external _ -> false
 
+let is_relocatable = function
+  | Unknown -> true
+  | Direct_primitive primitive -> Lam_primitive.is_relocatable primitive
+  | Direct_external { relocatable; _ } -> relocatable
+
 let params_match_args (params : Ident.t list) (args : Lam.t list) =
   List.same_length params args
   && List.for_all2 params args ~f:(fun param arg ->
-         match arg with
-         | Lam.Lvar ident | Lam.Lmutvar ident -> Ident.same param ident
-         | _ -> false)
+      match arg with
+      | Lam.Lvar ident | Lam.Lmutvar ident -> Ident.same param ident
+      | _ -> false)
 
 let rec of_lambda ~find_ident ~find_external lam =
   match lam with
@@ -39,7 +45,7 @@ let rec of_lambda ~find_ident ~find_external lam =
         args = [ Lam.Lglobal_module { id; dynamic_import } ];
         _;
       } ->
-      find_external ~dynamic_import id name
+      find_external ~dynamic_import id name ~arity:None
   | Lam.Lfunction { params; body; _ } ->
       of_eta_wrapper ~find_ident ~find_external params body
   | _ -> Unknown
@@ -48,8 +54,23 @@ and of_eta_wrapper ~find_ident ~find_external params body =
   match body with
   | Lam.Lprim { primitive; args; _ } when params_match_args params args ->
       Direct_primitive primitive
-  | Lam.Lapply { ap_func; ap_args; _ }
-    when params_match_args params ap_args -> (
+  | Lam.Lapply
+      {
+        ap_func =
+          Lprim
+            {
+              primitive = Pfield (_, Fld_module { name });
+              args = [ Lglobal_module { id; dynamic_import } ];
+              _;
+            };
+        ap_args;
+        _;
+      }
+    when params_match_args params ap_args ->
+      let arity = Lam_arity.info [ List.length params ] false in
+      find_external ~dynamic_import id name ~arity:(Some arity)
+  | Lam.Lapply { ap_func; ap_args; _ } when params_match_args params ap_args
+    -> (
       match of_lambda ~find_ident ~find_external ap_func with
       | (Direct_primitive _ | Direct_external _) as summary -> summary
       | Unknown -> Unknown)
