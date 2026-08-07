7f89993cf30e4104259613a89f025da92f8ba49f
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2026-08-06 20:55:21 -0700

    fix: handle module field namespace clashes

--- jscomp/core/js_dump_import_export.mli
+++ jscomp/core/js_dump_import_export.mli
@@ -24,8 +24,11 @@
 
 open Import
 
-val module_exports : Js_pp.Scope.t -> Js_pp.t -> Ident.t list -> Js_pp.Scope.t
-val es6_export : Js_pp.Scope.t -> Js_pp.t -> Ident.t list -> Js_pp.Scope.t
+val module_exports :
+  Js_pp.Scope.t -> Js_pp.t -> Runtime_fields.t list -> Js_pp.Scope.t
+
+val es6_export :
+  Js_pp.Scope.t -> Js_pp.t -> Runtime_fields.t list -> Js_pp.Scope.t
 
 type module_ = { id : Ident.t; path : string; default : bool }
 
