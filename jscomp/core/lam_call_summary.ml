d5095d375d2978aee42518b2243510ffed1f9637
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2026-09-13 16:01:54 -0700

    refactor(core): track matching parameter arity

--- jscomp/core/lam_call_summary.ml
+++ jscomp/core/lam_call_summary.ml
@@ -29,12 +29,16 @@ let is_relocatable = function
   | Direct_primitive primitive -> Lam_primitive.is_relocatable primitive
   | Direct_external { relocatable; _ } -> relocatable
 
-let params_match_args (params : Ident.t list) (args : Lam.t list) =
-  List.same_length params args
-  && List.for_all2 params args ~f:(fun param arg ->
-      match arg with
-      | Lam.Lvar ident | Lam.Lmutvar ident -> Ident.same param ident
-      | _ -> false)
+let params_matching_arity (params : Ident.t list) (args : Lam.t list) =
+  let rec loop arity params args =
+    match (params, args) with
+    | [], [] -> Some arity
+    | param :: params, (Lam.Lvar ident | Lam.Lmutvar ident) :: args
+      when Ident.same param ident ->
+        loop (arity + 1) params args
+    | _ -> None
+  in
+  loop 0 params args
 
 let rec of_lambda ~find_ident ~find_external lam =
   match lam with
@@ -51,27 +55,32 @@ let rec of_lambda ~find_ident ~find_external lam =
   | _ -> Unknown
 
 and of_eta_wrapper ~find_ident ~find_external params body =
-  match body with
-  | Lam.Lprim { primitive; args; _ } when params_match_args params args ->
-      Direct_primitive primitive
-  | Lam.Lapply
-      {
-        ap_func =
-          Lprim
-            {
-              primitive = Pfield (_, Fld_module { name });
-              args = [ Lglobal_module { id; dynamic_import } ];
-              _;
-            };
-        ap_args;
-        _;
-      }
-    when params_match_args params ap_args ->
-      let arity = Lam_arity.info [ List.length params ] false in
-      find_external ~dynamic_import id name ~arity:(Some arity)
-  | Lam.Lapply { ap_func; ap_args; _ } when params_match_args params ap_args
-    -> (
-      match of_lambda ~find_ident ~find_external ap_func with
-      | (Direct_primitive _ | Direct_external _) as summary -> summary
-      | Unknown -> Unknown)
-  | _ -> Unknown
+  let matching_arity =
+    match body with
+    | Lam.Lprim { args; _ } -> params_matching_arity params args
+    | Lam.Lapply { ap_args; _ } -> params_matching_arity params ap_args
+    | _ -> None
+  in
+  match matching_arity with
+  | None -> Unknown
+  | Some matching_arity -> (
+      match body with
+      | Lam.Lprim { primitive; _ } -> Direct_primitive primitive
+      | Lam.Lapply
+          {
+            ap_func =
+              Lprim
+                {
+                  primitive = Pfield (_, Fld_module { name });
+                  args = [ Lglobal_module { id; dynamic_import } ];
+                  _;
+                };
+            _;
+          } ->
+          let arity = Lam_arity.info [ matching_arity ] false in
+          find_external ~dynamic_import id name ~arity:(Some arity)
+      | Lam.Lapply { ap_func; _ } -> (
+          match of_lambda ~find_ident ~find_external ap_func with
+          | (Direct_primitive _ | Direct_external _) as summary -> summary
+          | Unknown -> Unknown)
+      | _ -> Unknown)
