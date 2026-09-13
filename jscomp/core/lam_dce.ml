a450be7048def2564fc98b68b7b87d541c707d78
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2026-09-13 12:40:05 -0700

    refactor(core): concatenate lookup diagnostics

--- jscomp/core/lam_dce.ml
+++ jscomp/core/lam_dce.ml
@@ -32,9 +32,10 @@ let transitive_closure (initial_idents : Ident.t list)
       Ident.Hashtbl.replace visited ~key:id ~data:();
       match Ident.Hashtbl.find ident_freevars id with
       | exception Not_found ->
-          Format.ksprintf
-            (fun s -> failwith (__LOC__ ^ s))
-            "%s/%d not found" (Ident.name id) (Ident.stamp id)
+          failwith
+            (__LOC__ ^ Ident.name id ^ "/"
+            ^ string_of_int (Ident.stamp id)
+            ^ " not found")
       | e -> Ident.Set.iter ~f:dfs e)
   in
   List.iter ~f:dfs initial_idents;
