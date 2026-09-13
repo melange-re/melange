6e3d598cfc9a309221d12d94bdf3d61cf17c3895
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2026-09-13 12:36:45 -0700

    refactor(core): concatenate preprocessor commands

--- jscomp/core/pparse_driver.ml
+++ jscomp/core/pparse_driver.ml
@@ -3,9 +3,7 @@ open Import
 (* Optionally preprocess a source file *)
 let call_external_preprocessor sourcefile pp =
   let tmpfile = Filename.temp_file "ocamlpp" "" in
-  let comm =
-    Printf.sprintf "%s %s > %s" pp (Filename.quote sourcefile) tmpfile
-  in
+  let comm = pp ^ " " ^ Filename.quote sourcefile ^ " > " ^ tmpfile in
   if Ccomp.command comm <> 0 then (
     Misc.remove_file tmpfile;
     Cmd_ast_exception.cannot_run comm);
