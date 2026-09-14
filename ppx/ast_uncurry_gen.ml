13825da0efaf025abab70fbbafea6882e272a974
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2026-09-13 17:34:54 -0700

    refactor(ppx): track uncurried function arity (#1918)
    * refactor(ppx): track uncurried function arity
    
    * refactor(ppx): hoist argument traversal helper
    
    * refactor(ppx): encapsulate function argument traversal

--- ppx/ast_uncurry_gen.ml
+++ ppx/ast_uncurry_gen.ml
@@ -25,22 +25,28 @@
 open Import
 open Ast_helper
 
-let process_args ~loc self args ~init =
-  List.fold_left ~init args ~f:(fun acc param ->
-      match param with
-      | { pparam_desc = Pparam_newtype _; _ } -> acc
-      | { pparam_desc = Pparam_val (arg_label, _, arg); _ } ->
-          Error.optional_err ~loc arg_label;
-          (arg_label, self#pattern arg) :: acc)
-
-let rec aux ~loc self acc (body : expression) =
-  match Ast_attributes.process_attributes_rev body.pexp_attributes with
-  | Nothing, _ -> (
-      match body.pexp_desc with
-      | Pexp_function (args, _, Pfunction_body body) ->
-          aux ~loc self (process_args ~loc self args ~init:acc) body
-      | _ -> (self#expression body, acc))
-  | _, _ -> (self#expression body, acc)
+let process_function =
+  let rec process_args ~loc self args ~arity rev_args =
+    match args with
+    | [] -> (arity, rev_args)
+    | { pparam_desc = Pparam_newtype _; _ } :: args ->
+        process_args ~loc self args ~arity rev_args
+    | { pparam_desc = Pparam_val (arg_label, _, arg); _ } :: args ->
+        Error.optional_err ~loc arg_label;
+        process_args ~loc self args ~arity:(arity + 1)
+          ((arg_label, self#pattern arg) :: rev_args)
+  in
+  let rec aux ~loc self ((arity, rev_args) as acc) (body : expression) =
+    match Ast_attributes.process_attributes_rev body.pexp_attributes with
+    | Nothing, _ -> (
+        match body.pexp_desc with
+        | Pexp_function (args, _, Pfunction_body body) ->
+            aux ~loc self (process_args ~loc self args ~arity rev_args) body
+        | _ -> (self#expression body, acc))
+    | _, _ -> (self#expression body, acc)
+  in
+  fun ~loc self args body ->
+    aux ~loc self (process_args ~loc self args ~arity:0 []) body
 
 (* Handling `fun [@this]` used in `object [@u] end` *)
 let to_method_callback =
@@ -67,10 +73,9 @@ let to_method_callback =
     let first_arg = self#pattern (first_arg args) in
     if not (is_single_variable_pattern_conservative first_arg) then
       Error.err ~loc:first_arg.ppat_loc Mel_this_simple_pattern;
-    let body, rev_extra_args =
-      let result, rev_extra_args =
-        let rev_args = process_args ~loc self args ~init:[] in
-        aux ~loc self rev_args body
+    let body, arity =
+      let result, (arity, rev_extra_args) =
+        process_function ~loc self args body
       in
       let body =
         Ast_builder.Default.pexp_function ~loc
@@ -80,9 +85,8 @@ let to_method_callback =
              rev_extra_args)
           None (Pfunction_body result)
       in
-      (body, rev_extra_args)
+      (body, arity)
     in
-    let arity = List.length rev_extra_args in
     let arity_s = string_of_int arity in
     Pexp_apply
       ( Exp.ident ~loc { loc; txt = Ast_literal.unsafe_to_method },
@@ -101,17 +105,14 @@ let to_method_callback =
         ] )
 
 let to_uncurry_fn ~loc (self : Ast_traverse.map) args body : expression_desc =
-  let result, rev_extra_args =
-    let rev_args = process_args ~loc self args ~init:[] in
-    aux ~loc self rev_args body
-  in
+  let result, (arity, rev_extra_args) = process_function ~loc self args body in
   let arity =
     match rev_extra_args with
     | [
      (_, { ppat_desc = Ppat_construct ({ txt = Lident "()"; _ }, None); _ });
     ] ->
         0
-    | _ -> List.length rev_extra_args
+    | _ -> arity
   in
   Error.err_large_arity ~loc arity;
   let body =
