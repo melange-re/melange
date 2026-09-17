0d44bd2448775fc1a00310364bf48fac5715f7a7
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2026-09-17 11:41:51 -0700

    refactor(ppx): join timing label parts with String.concat

--- ppx/melange_ppx.cppo.ml
+++ ppx/melange_ppx.cppo.ml
@@ -225,7 +225,8 @@ module Time = struct
                     loc_start.pos_lnum,
                     loc_start.pos_cnum - loc_start.pos_bol )
                 in
-                Filename.basename file ^ " " ^ string_of_int lnum
+                String.concat ~sep:" "
+                  [ Filename.basename file; string_of_int lnum ]
             in
             Exp.sequence ~loc
               [%expr
